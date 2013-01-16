ecc08cce3808ab440c8845c283c3a97fd975aacd
Lofty Mettyer <COA\lofty.mettyer>
2013-01-16 15:16:31 +0000

    jira-2780

--- Web Apps/DMI.NET/DMI.NET/Code/ASRIntranetFunctions.vb
+++ Web Apps/DMI.NET/DMI.NET/Code/ASRIntranetFunctions.vb
@@ -2,59 +2,64 @@
 
 Public Module ASRIntranetFunctions
 
-	'TODO
-	Function GetRegistrySetting(psAppName As String, psSection As String, psKey As String) As String
-		' Get the required value from the registry with the given registry key values.
-		GetRegistrySetting = GetSetting(AppName:=psAppName, Section:=psSection, Key:=psKey)
+  'TODO
+  Public Function GetRegistrySetting(psAppName As String, psSection As String, psKey As String) As String
+    ' Get the required value from the registry with the given registry key values.
+    GetRegistrySetting = GetSetting(AppName:=psAppName, Section:=psSection, Key:=psKey)
 
-	End Function
+  End Function
 
-	'TODO
-	Function LocaleDateFormat() As String
+  'TODO
+  Function LocaleDateFormat() As String
 
-		Dim sLocaleDateFormat As String = Thread.CurrentThread.CurrentUICulture.DateTimeFormat.ShortDatePattern.ToLower()
+    Dim sLocaleDateFormat As String = Thread.CurrentThread.CurrentUICulture.DateTimeFormat.ShortDatePattern.ToLower()
 
-		Return sLocaleDateFormat
+    Return sLocaleDateFormat
 
-	End Function
+  End Function
 
-	'TODO
-	Function LocaleDecimalSeparator() As String
-		Return ""
-	End Function
+  'TODO
+  Function LocaleDecimalSeparator() As String
+    Return ""
+  End Function
 
-	'TODO
-	Function LocaleThousandSeparator() As String
-		Return ""
-	End Function
+  'TODO
+  Function LocaleThousandSeparator() As String
+    Return ""
+  End Function
 
-	'TODO
-	Function LocaleDateSeparator() As String
-		Return ""
-	End Function
+  'TODO
+  Function LocaleDateSeparator() As String
+    Return ""
+  End Function
 
-	'****************************************************************
-	' NullSafeString
-	'****************************************************************
-	Public Function NullSafeString(ByVal arg As Object, _
-	Optional ByVal returnIfEmpty As String = "") As String
+  '****************************************************************
+  ' NullSafeString
+  '****************************************************************
+  Public Function NullSafeString(ByVal arg As Object, _
+  Optional ByVal returnIfEmpty As String = "") As String
 
-		Dim returnValue As String
+    Dim returnValue As String
 
-		If (arg Is DBNull.Value) OrElse (arg Is Nothing) _
-		  OrElse (arg Is String.Empty) Then
-			returnValue = returnIfEmpty
-		Else
-			Try
-				returnValue = CStr(arg)
-			Catch
-				returnValue = returnIfEmpty
-			End Try
+    If (arg Is DBNull.Value) OrElse (arg Is Nothing) _
+      OrElse (arg Is String.Empty) Then
+      returnValue = returnIfEmpty
+    Else
+      Try
+        returnValue = CStr(arg)
+      Catch
+        returnValue = returnIfEmpty
+      End Try
 
-		End If
+    End If
 
-		Return returnValue
+    Return returnValue
 
-	End Function
+  End Function
+
+  ' TODO
+  Function ValidateDir(ByRef paramType As String) As Boolean
+    Return True
+  End Function
 
 End Module
