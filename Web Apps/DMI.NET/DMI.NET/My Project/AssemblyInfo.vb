85cb0c3704c4c6cbe728589596aceb742aae1ef8
Nick Gibson <COA\nick.gibson>
2013-01-28 09:28:15 +0000

    Version 8.0.0.2

--- Web Apps/DMI.NET/DMI.NET/My Project/AssemblyInfo.vb
+++ Web Apps/DMI.NET/DMI.NET/My Project/AssemblyInfo.vb
@@ -9,9 +9,9 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 <Assembly: AssemblyTitle("DMI.NET")> 
 <Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyCompany("Advanced Computer Software Ltd")> 
 <Assembly: AssemblyProduct("DMI.NET")> 
-<Assembly: AssemblyCopyright("Copyright © Microsoft 2012")> 
+<Assembly: AssemblyCopyright("Copyright © 2012")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("8.0.0.2")> 
+<Assembly: AssemblyFileVersion("8.0.0.2")> 
