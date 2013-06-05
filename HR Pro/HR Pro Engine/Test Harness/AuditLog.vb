afd08914c81731f90807f6d189296eaee60a0177
SourceSafeUser Paul.glynn <SourceSafeUser Paul.glynn>
2013-06-05 20:17:36 +0100

    {27/10/2011 11:53:54~27/10/2011 11:53:56}

--- HR Pro/HR Pro Engine/Test Harness/AuditLog.vb
+++ HR Pro/HR Pro Engine/Test Harness/AuditLog.vb
@@ -4,21 +4,30 @@
 
   Private Sub AuditLog_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
-    Dim obj As New DataSet
+        Dim db As New DBContextDataContext
 
-    grdAudit.DataSource = Database.GetAuditLogDataSource
-    '    grdAudit.DataSource = Database.GetAuditLogDescriptions
+        Dim data = From a In db.ASRSysAuditTrails
+                   Select a
+
+        Dim sw As New System.Diagnostics.Stopwatch
+
+        sw.Start()
+
+        grdAudit.DataSource = data.ToList()
+
+        Console.WriteLine(sw.ElapsedMilliseconds)
 
   End Sub
 
-  Private Sub grdAudit_InitializeLayout1(ByVal sender As Object, ByVal e As Infragistics.Win.UltraWinGrid.InitializeLayoutEventArgs) Handles grdAudit.InitializeLayout
-    e.Layout.Override.FilterUIType = Infragistics.Win.UltraWinGrid.FilterUIType.HeaderIcons
+    Private Sub grdAudit_InitializeLayout1(ByVal sender As Object, ByVal e As Infragistics.Win.UltraWinGrid.InitializeLayoutEventArgs) Handles grdAudit.InitializeLayout
 
-  End Sub
+        e.Layout.Override.FilterUIType = Infragistics.Win.UltraWinGrid.FilterUIType.HeaderIcons
+
+    End Sub
 
   Private Sub butOutput_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles butOutput.Click
 
-    UltraGridExcelExporter1.Export(grdAudit, txtFilePath.Text)
+        UltraGridExcelExporter1.Export(grdAudit, txtFilePath.Text)
 
   End Sub
 End Class
