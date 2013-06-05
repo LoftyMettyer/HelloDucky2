0e42e30abc8600f76d981f5653cbd37de8efd540
SourceSafeUser Paul.glynn <SourceSafeUser Paul.glynn>
2013-06-05 20:30:35 +0100

    {05/12/2011 11:07:58}

--- HR Pro/HR Pro Engine/Engine/Database Scripting/General.vb
+++ HR Pro/HR Pro Engine/Engine/Database Scripting/General.vb
@@ -3,53 +3,7 @@
   <HideModuleName()>
   Public Module General
 
-    Public Function DropUDF(ByVal [Role] As String, ByVal [ObjectName] As String) As Boolean
 
-      Dim sSQL As String = String.Empty
 
-      Try
-
-        sSQL = String.Format("IF EXISTS(SELECT o.[name] FROM sys.sysobjects o " & _
-          "INNER JOIN sys.sysusers u ON o.[uid] = u.[uid] " & _
-          "WHERE o.[name] = '{1}' AND [type] IN ('FN', 'TF') AND u.[name] = '{0}')" & vbNewLine & _
-          " DROP FUNCTION [{0}].[{1}]", [Role], [ObjectName])
-
-        ' Commit
-        CommitDB.ScriptStatement(sSQL)
-
-      Catch ex As Exception
-        Globals.ErrorLog.Add(SystemFramework.ErrorHandler.Section.UDFs, [ObjectName], SystemFramework.ErrorHandler.Severity.Error, ex.Message, sSQL)
-        Return False
-
-      End Try
-
-      Return True
-
-    End Function
-
-    Public Function DropProcedure(ByVal [Role] As String, ByVal [ObjectName] As String) As Boolean
-
-      Dim sSQL As String = String.Empty
-
-      Try
-
-        sSQL = String.Format("IF EXISTS(SELECT o.[name] FROM sys.sysobjects o " & _
-          "INNER JOIN sys.sysusers u ON o.[uid] = u.[uid] " & _
-          "WHERE o.[name] = '{1}' AND [type] = 'P' AND u.[name] = '{0}')" & vbNewLine & _
-          " DROP PROCEDURE [{0}].[{1}]", [Role], [ObjectName])
-
-        ' Commit
-        CommitDB.ScriptStatement(sSQL)
-
-      Catch ex As Exception
-        Globals.ErrorLog.Add(SystemFramework.ErrorHandler.Section.General, [ObjectName], SystemFramework.ErrorHandler.Severity.Error, ex.Message, sSQL)
-        Return False
-
-      End Try
-
-      Return True
-
-    End Function
-
-  End Module
+   End Module
 End Namespace
