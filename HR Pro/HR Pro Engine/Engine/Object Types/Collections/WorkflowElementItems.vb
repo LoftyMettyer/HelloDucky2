e1286ad8ce594c315286d9ae4aa0e60b0b234ff7
SourceSafeUser Paul.glynn <SourceSafeUser Paul.glynn>
2013-06-05 20:21:12 +0100

    {31/10/2011 13:49:42}

--- HR Pro/HR Pro Engine/Engine/Object Types/Collections/WorkflowElementItems.vb
+++ HR Pro/HR Pro Engine/Engine/Object Types/Collections/WorkflowElementItems.vb
@@ -8,23 +8,23 @@ Namespace Things.Collections
     Inherits Things.Collections.BaseCollection
     Implements iCollection_WorkflowElements
 
-    Public Function Element(ByRef [ID] As HCMGuid) As WorkflowElement Implements COMInterfaces.iCollection_WorkflowElements.Element
+        Public Function Element(ByRef [ID] As HCMGuid) As WorkflowElement Implements COMInterfaces.iCollection_WorkflowElements.Element
 
-      Dim objChild As Things.Base
+            Dim objChild As Things.Base
 
-      For Each objChild In MyBase.Items
-        If objChild.ID = ID And objChild.Type = Type.WorkflowElementItem Then
-          Return objChild
-        End If
-      Next
+            For Each objChild In MyBase.Items
+                If objChild.ID = ID And objChild.Type = Type.WorkflowElementItem Then
+                    Return objChild
+                End If
+            Next
 
-      Return Nothing
+            Return Nothing
 
-    End Function
+        End Function
 
-    Public Function Elements() As BaseCollection Implements COMInterfaces.iCollection_WorkflowElements.Elements
-      Return Me.Items
-    End Function
+        Public Function Elements() As BaseCollection Implements COMInterfaces.iCollection_WorkflowElements.Elements
+            Return Me.Items
+        End Function
 
     Public Shadows Sub Add(ByRef objWorkflowElementItem As Things.WorkflowElementItem) Implements COMInterfaces.iCollection_WorkflowElements.Add
       Me.Items.Add(objWorkflowElementItem)
