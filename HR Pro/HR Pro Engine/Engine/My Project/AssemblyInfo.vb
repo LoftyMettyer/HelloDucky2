9bb42edb06814c05ec383485fc050e36486adc4e
SourceSafeUser Harpdev01 <SourceSafeUser Harpdev01>
2013-06-05 21:28:45 +0100

    {01/06/2012 10:29:00}

--- HR Pro/HR Pro Engine/Engine/My Project/AssemblyInfo.vb
+++ HR Pro/HR Pro Engine/Engine/My Project/AssemblyInfo.vb
@@ -31,7 +31,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("5.0.16.0")> 
+<Assembly: AssemblyVersion("5.0.17.0")> 
 
 
-<Assembly: AssemblyFileVersionAttribute("5.0.16.0")> 
+<Assembly: AssemblyFileVersionAttribute("5.0.17.0")> 
