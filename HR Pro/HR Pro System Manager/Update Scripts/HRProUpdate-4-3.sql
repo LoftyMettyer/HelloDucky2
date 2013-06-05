8e63c0195ed63560759e1632c7d6f5c5746d4396
SourceSafeUser Harpdev01 <SourceSafeUser Harpdev01>
2013-06-05 20:10:36 +0100

    {12/09/2011 10:57:42~12/09/2011 10:59:54}

--- HR Pro/HR Pro System Manager/Update Scripts/HRProUpdate-4-3.sql
+++ HR Pro/HR Pro System Manager/Update Scripts/HRProUpdate-4-3.sql
@@ -3051,7 +3051,7 @@ PRINT 'Final Step - Updating Versions'
 	EXEC spsys_setsystemsetting '.NET Assembly', 'minimum version', '4.2.0';
 	EXEC spsys_setsystemsetting 'outlook service', 'minimum version', '4.2.0';
 	EXEC spsys_setsystemsetting 'workflow service', 'minimum version', '4.2.0';
-	EXEC spsys_setsystemsetting 'system framework', 'version', '1.0.4272.15412';
+	EXEC spsys_setsystemsetting 'system framework', 'version', '1.0.4272.17857';
 
 
 insert into asrsysauditaccess
