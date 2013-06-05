d11d02ed9ad1198871b01e29c8ba370db6feb62e
SourceSafeUser Lofty.mettyer <SourceSafeUser Lofty.mettyer>
2013-06-05 20:14:23 +0100

    {19/09/2011 11:16:06}JIRA-1768 - Product rename

--- HR Pro/HR Pro System Manager/Update Scripts/HRProUpdate-5-0.sql
+++ HR Pro/HR Pro System Manager/Update Scripts/HRProUpdate-5-0.sql
@@ -64,6 +64,15 @@ PRINT 'Step 1 - System procedures'
 	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[sp_ASRAuditTable]') AND xtype = 'P')
 		DROP PROCEDURE [dbo].[sp_ASRAuditTable];
 
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[sp_ASRAllTablePermissions]') AND xtype = 'P')
+		DROP PROCEDURE [dbo].[sp_ASRAllTablePermissions];
+
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[sp_ASRAllTablePermissionsForGroup]') AND xtype = 'P')
+		DROP PROCEDURE [dbo].[sp_ASRAllTablePermissionsForGroup];
+
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[sp_ASRFn_GetCurrentUser]') AND xtype = 'P')
+		DROP PROCEDURE [dbo].[sp_ASRFn_GetCurrentUser];
+
 	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spASRDelegateWorkflowEmail]') AND xtype = 'P')
 		DROP PROCEDURE [dbo].[spASRDelegateWorkflowEmail];
 
@@ -85,7 +94,106 @@ PRINT 'Step 1 - System procedures'
 	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spstat_getaudittrail]') AND xtype = 'P')
 		DROP PROCEDURE [dbo].[spstat_getaudittrail];
 
-		
+
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[sp_ASRAllTablePermissions] 
+	(
+		@psSQLLogin		varchar(200)
+	)
+	AS
+	BEGIN
+
+		SET NOCOUNT ON;
+
+		/* Return parameters showing what permissions the current user has on all of the tables. */
+		DECLARE @iUserGroupID	int;
+
+		/* Initialise local variables. */
+		SELECT @iUserGroupID = usg.gid
+		FROM sysusers usu
+		left outer join
+		(sysmembers mem inner join sysusers usg on mem.groupuid = usg.uid) on usu.uid = mem.memberuid
+		WHERE (usu.islogin = 1 and usu.isaliased = 0 and usu.hasdbaccess = 1) and
+			(usg.issqlrole = 1 or usg.uid is null) and
+			usu.name = @psSQLLogin AND not (usg.name like ''ASRSys%'');
+
+		-- Cached cut down view of the sysprotects table
+		DECLARE @SysProtects TABLE([ID] int, [Action] tinyint, [ProtectType] tinyint, [Columns] varbinary(8000))
+		INSERT @SysProtects
+			SELECT [ID],[Action],[ProtectType], [Columns] FROM sysprotects
+			WHERE [UID] = @iUserGroupID;
+
+		-- Cached version of the Base table IDs
+		DECLARE @BaseTableIDs TABLE([ID] int PRIMARY KEY CLUSTERED, [BaseTableID] int)
+		INSERT @BaseTableIDs
+			SELECT DISTINCT o.ID, v.TableID
+			FROM sysobjects o
+			INNER JOIN dbo.ASRSysChildViews2 v ON v.ChildViewID = CONVERT(integer,SUBSTRING(o.Name,9,PATINDEX ( ''%#%'' , o.Name) - 9))
+			WHERE Name LIKE ''ASRSYSCV%'';
+
+
+		SELECT o.name, p.action, bt.BaseTableID
+		FROM @SysProtects p
+		INNER JOIN sysobjects o ON p.id = o.id
+		LEFT OUTER JOIN @BaseTableIDs bt ON o.id = bt.id
+		WHERE p.protectType <> 206
+			AND p.action <> 193
+			AND (o.xtype = ''u'' or o.xtype = ''v'')
+			AND (o.Name NOT LIKE ''ASRSYS%'' OR o.Name LIKE ''ASRSYSCV%'')
+		UNION
+		SELECT o.name, 193, bt.BaseTableID
+		FROM syscolumns
+		INNER JOIN @SysProtects p ON (syscolumns.id = p.id
+			AND p.action = 193 
+			AND (((convert(tinyint,substring(p.columns,1,1))&1) = 0
+			AND (convert(int,substring(p.columns,sysColumns.colid/8+1,1))&power(2,sysColumns.colid&7)) != 0)
+			OR ((convert(tinyint,substring(p.columns,1,1))&1) != 0
+			AND (convert(int,substring(p.columns,sysColumns.colid/8+1,1))&power(2,sysColumns.colid&7)) = 0)))
+		INNER JOIN sysobjects o ON o.id = p.id
+		LEFT OUTER JOIN @BaseTableIDs bt ON o.id = bt.id
+		WHERE syscolumns.name = ''timestamp''
+			AND p.protectType IN (204, 205) 
+			AND (o.Name NOT LIKE ''ASRSYS%'' OR o.Name LIKE ''ASRSYSCV%'')
+		ORDER BY o.name;
+
+	END'
+
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[sp_ASRAllTablePermissionsForGroup]
+	(
+		@psGroupName sysname
+	)
+	AS
+	BEGIN
+		-- Return parameters showing what permissions the current user has on all of the tables.
+		DECLARE @iUserGroupID	integer;
+
+		-- Initialise local variables.
+		SELECT @iUserGroupID = sysusers.gid
+		FROM sysusers
+		WHERE sysusers.name = @psGroupName;
+
+		SELECT sysobjects.name, sysprotects.action
+		FROM sysprotects 
+		INNER JOIN sysobjects ON sysprotects.id = sysobjects.id
+		WHERE sysprotects.uid = @iUserGroupID
+			AND sysprotects.protectType <> 206
+			AND (sysobjects.xtype = ''u'' or sysobjects.xtype = ''v'')
+			AND (sysobjects.Name NOT LIKE ''ASRSYS%'' OR sysobjects.Name LIKE ''ASRSYSCV%'')
+		ORDER BY sysobjects.name;
+	
+	END'		
+
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[sp_ASRFn_GetCurrentUser]
+	(
+		@psResult	varchar(255) OUTPUT
+	)
+	AS
+	BEGIN
+		SET @psResult = 
+			CASE 
+				WHEN UPPER(LEFT(APP_NAME(), 15)) = ''OPENHR WORKFLOW'' THEN ''OpenHR Workflow'' 
+				ELSE SUSER_SNAME()
+			END;
+	END'
 
 	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spstat_setdefaultsystemsetting](
 			@section AS varchar(50),
@@ -309,7 +417,6 @@ PRINT 'Step 1 - System procedures'
 		END
 	END'
 
-
 	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spASRGetCurrentUsersAppName]
 	(
 		@psAppName		varchar(MAX) OUTPUT,
@@ -338,7 +445,6 @@ PRINT 'Step 1 - System procedures'
 		ORDER BY p.loginame;
 
 	END'
-
 
 	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spASRGetCurrentUsersCountOnServer]
 	(
@@ -417,7 +523,6 @@ PRINT 'Step 1 - System procedures'
 			, @pwdHistoryLength OUT, @pwdProperties OUT;
 
 	END';
-
 
 	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spASRSubmitWorkflowStep]
 	(
@@ -1298,7 +1403,6 @@ PRINT 'Step 1 - System procedures'
 		END
 	END'
 
-
 	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spstat_audittable] (
 		@piTableID int,
 		@piRecordID int,
@@ -1430,7 +1534,6 @@ PRINT 'Step 1 - System procedures'
 		IF LEN(@sExecString) > 0 EXECUTE sp_executeSQL @sExecString;
 
 	END'
-
 
 
 
