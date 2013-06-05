55f3cf63f1548b336ca05a706b1fd29e292bbe3b
SourceSafeUser Mike.hempenstall <SourceSafeUser Mike.hempenstall>
2013-06-05 15:01:41 +0100

    {24/03/2010 10:07:04}

--- HR Pro/HR Pro System Manager/Update Scripts/HRProUpdate-4-1.sql
+++ HR Pro/HR Pro System Manager/Update Scripts/HRProUpdate-4-1.sql
@@ -1797,70 +1797,70 @@ END';
 		END';
 	EXECUTE sp_executeSQL @sSPCode;
 
-	SET @sSPCode = 'ALTER PROCEDURE [dbo].[spASRIntGetSSIWelcomeDetails]	
+	SET @sSPCode = 'ALTER PROCEDURE [dbo].[spASRIntGetSSIWelcomeDetails]  
 (
-		@piWelcomeColumnID integer,
-		@piSingleRecordViewID integer,
-		@psUserName varchar(255),	
-		@psWelcomeMessage varchar(255) OUTPUT
+            @piWelcomeColumnID integer,
+            @piSingleRecordViewID integer,
+            @psUserName varchar(255),     
+            @psWelcomeMessage varchar(255) OUTPUT
 )
 AS
 BEGIN
 
-	DECLARE @sql nvarchar(max)
-	DECLARE @dtLastLogon datetime
-	DECLARE @myval varchar(max)
-	DECLARE @psLogonTime varchar(20)
-	DECLARE @psLogonDay varchar(20)
-	DECLARE @psWelcomeName varchar(255)
-	DECLARE @psLastLogon varchar(50)		
+      DECLARE @sql nvarchar(max)
+      DECLARE @dtLastLogon datetime
+      DECLARE @myval varchar(max)
+      DECLARE @psLogonTime varchar(20)
+      DECLARE @psLogonDay varchar(20)
+      DECLARE @psWelcomeName varchar(255)
+      DECLARE @psLastLogon varchar(50)          
 
-	--- try to get the users welcome name
+      --- try to get the users welcome name
 
-	BEGIN TRY
-		SELECT @sql = ''SELECT @outparm = [''+c.columnname+''] FROM [''+v.viewname+'']''
-			FROM ASRSysColumns c, ASRSysViews v
-			WHERE c.columnID = @piWelcomeColumnID AND v.ViewID = @piSingleRecordViewID
+      BEGIN TRY
+            SELECT @sql = ''SELECT @outparm = [''+c.columnname+''] FROM [''+v.viewname+'']''
+                  FROM ASRSysColumns c, ASRSysViews v
+                  WHERE c.columnID = @piWelcomeColumnID AND v.ViewID = @piSingleRecordViewID
 
-		EXEC sp_executesql @sql, N''@outparm nvarchar(max) output'', @myval OUTPUT
-	
-		IF LEN(LTRIM(RTRIM(@myval))) = 0 OR @@ROWCOUNT = 0 or ISNULL(@myval, '''') = ''''
-		BEGIN
-			SET @psWelcomeName = ''''
-		END
-		ELSE
-		BEGIN
-			SET @psWelcomeName = '' '' + isnull(@myval, '''')
-		END
+            EXEC sp_executesql @sql, N''@outparm nvarchar(max) output'', @myval OUTPUT
+      
+            IF LEN(LTRIM(RTRIM(@myval))) = 0 OR @@ROWCOUNT = 0 or ISNULL(@myval, '''') = ''''
+            BEGIN
+                  SET @psWelcomeName = ''''
+            END
+            ELSE
+            BEGIN
+                  SET @psWelcomeName = '' '' + isnull(@myval, '''')
+            END
 
-	END TRY
-	
-	BEGIN CATCH
-		SET @psWelcomeName = ''''
-	END CATCH
-	
-	--- Now get the last logon details
+      END TRY
+      
+      BEGIN CATCH
+            SET @psWelcomeName = ''''
+      END CATCH
+      
+      --- Now get the last logon details
 
-	SELECT top 2 @dtLastLogon = DateTimeStamp
-		FROM ASRSysAuditAccess WHERE [UserName] = @psUserName
-		AND [HRProModule] = ''Intranet'' AND [Action] = ''log in''
-	ORDER BY DateTimeStamp DESC
+      SELECT top 2 @dtLastLogon = DateTimeStamp
+            FROM ASRSysAuditAccess WHERE [UserName] = @psUserName
+            AND [HRProModule] = ''Intranet'' AND [Action] = ''log in''
+      ORDER BY DateTimeStamp DESC
 
-	IF @@ROWCOUNT > 0 
-	BEGIN
-		SET @psLogonTime = CONVERT(varchar(50), CONVERT(time(0), @dtLastLogon))
-		SELECT @psLogonDay = 
-			CASE datediff(day, @dtLastLogon, GETDATE())
-			WHEN 0 THEN ''today''
-			WHEN 1 THEN ''yesterday''
-			ELSE ''on '' + CAST(DAY(@dtLastLogon) AS VARCHAR(2)) + '' '' + DATENAME(MM, @dtLastLogon) + '' '' + CAST(YEAR(@dtLastLogon) AS VARCHAR(4))
-		END
-		SET @psWelcomeMessage = ''Welcome back'' + @psWelcomeName + '', you last logged in at '' + @psLogonTime + '' '' + @psLogonDay
-	END
-	ELSE
-	BEGIN
-		SET @psWelcomeMessage = ''Welcome '' + @psWelcomeName
-	END
+      IF @@ROWCOUNT > 0 
+      BEGIN
+            SET @psLogonTime = CONVERT(varchar(5), @dtLastLogon, 108)
+            SELECT @psLogonDay = 
+                  CASE datediff(day, @dtLastLogon, GETDATE())
+                  WHEN 0 THEN ''today''
+                  WHEN 1 THEN ''yesterday''
+                  ELSE ''on '' + CAST(DAY(@dtLastLogon) AS VARCHAR(2)) + '' '' + DATENAME(MM, @dtLastLogon) + '' '' + CAST(YEAR(@dtLastLogon) AS VARCHAR(4))
+            END
+            SET @psWelcomeMessage = ''Welcome back'' + @psWelcomeName + '', you last logged in at '' + @psLogonTime + '' '' + @psLogonDay
+      END
+      ELSE
+      BEGIN
+            SET @psWelcomeMessage = ''Welcome '' + @psWelcomeName
+      END
 
 END';
 
