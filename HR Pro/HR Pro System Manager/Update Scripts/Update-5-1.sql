5b57ffee83824bca6ed0492bba3738728213235a
SourceSafeUser Paul.glynn <SourceSafeUser Paul.glynn>
2013-06-05 21:42:28 +0100

    {01/08/2012 14:24:48~01/08/2012 14:25:28}

--- HR Pro/HR Pro System Manager/Update Scripts/Update-5-1.sql
+++ HR Pro/HR Pro System Manager/Update Scripts/Update-5-1.sql
@@ -1845,7 +1845,91 @@ PRINT 'Step - Menu & Category enhancements'
 
 	EXECUTE sp_executeSQL @sSPCode;
 
+/* ------------------------------------------------------------- */
+/* Step - Audit Log updates */
+/* ------------------------------------------------------------- */
+PRINT 'Step - Audit Log Updates'
 
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spstat_getaudittrail]') AND xtype = 'P')
+		DROP PROCEDURE [dbo].[spstat_getaudittrail];
+		
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spstat_getaudittrail] (
+		@piAuditType	int,
+		@psOrder 		varchar(MAX),
+		@psFilter		varchar(MAX),
+		@piTop			int)
+	AS
+	BEGIN
+
+		SET NOCOUNT ON;
+
+		DECLARE @sSQL nvarchar(MAX);
+
+		IF @piAuditType = 1
+		BEGIN
+			SET @sSQL = ''SELECT {TOP} 
+				a.userName AS [User], 
+				a.dateTimeStamp AS [Date / Time], 
+				a.tableName AS [Table], 
+				a.columnName AS [Column], 
+				a.oldValue AS [Old Value], 
+				a.newValue AS [New Value], 
+				a.recordDesc AS [Record Description],
+				a.id,
+				CASE WHEN c.DataType = 2 OR c.DataType = 4 THEN 1 ELSE 0 END AS IsNumeric
+				FROM dbo.ASRSysAuditTrail a
+				LEFT JOIN dbo.tbsys_columns c ON c.ColumnID = a.ColumnID'';
+		END
+		ELSE IF @piAuditType = 2
+			SET @sSQL =  ''SELECT {TOP} 
+				a.userName AS [User], 
+				a.dateTimeStamp AS [Date / Time],
+				a.groupName AS [User Group],
+				a.viewTableName AS [View / Table],
+				a.columnName AS [Column], 
+				a.action AS [Action],
+				a.permission AS [Permission], 
+				a.id
+				FROM dbo.ASRSysAuditPermissions a'';
+		ELSE IF @piAuditType = 3
+			SET @sSQL = ''SELECT {TOP} 
+				a.userName AS [User],
+    			a.dateTimeStamp AS [Date / Time],
+				a.groupName AS [User Group], 
+				a.userLogin AS [User Login],
+				a.[Action], 
+				a.id
+				FROM dbo.ASRSysAuditGroup a'';
+		ELSE IF @piAuditType = 4
+			SET @sSQL = ''SELECT {TOP} 
+				a.DateTimeStamp AS [Date / Time],
+				a.UserGroup AS [User Group],
+				a.UserName AS [User], 
+				a.ComputerName AS [Computer Name],
+				a.HRProModule AS [Module],
+				a.Action AS [Action], 
+				a.id
+				FROM dbo.ASRSysAuditAccess a'';
+				
+		IF LEN(@psFilter) > 0
+			SET @sSQL = @sSQL + CHAR(10) + ''WHERE '' + @psFilter;
+
+		IF LEN(@psOrder) > 0
+			SET @sSQL = @sSQL + CHAR(10) + @psOrder;
+				
+		-- Retreive selected data
+		IF LEN(@sSQL) > 0 
+		BEGIN
+			IF ISNULL(@piTop, 0) > 0
+				SET @sSQL = REPLACE(@sSQL, ''{TOP}'', ''TOP '' + convert(varchar, @piTop));
+				
+			EXECUTE sp_executeSQL @sSQL;
+		END
+
+	END';
+
+	IF NOT EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[dbo].[ASRSysAuditTrail]') AND name = N'IDX_DateTimeStamp')
+		EXEC sp_executesql N'CREATE NONCLUSTERED INDEX [IDX_DateTimeStamp] ON [dbo].[ASRSysAuditTrail] ([DateTimeStamp] ASC)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]';
 
 /* ------------------------------------------------------------- */
 /* Step - Image updates */
