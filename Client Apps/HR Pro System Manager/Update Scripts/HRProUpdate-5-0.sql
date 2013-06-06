51a4aa83d8b3c6b8a4897ab01813292c67b488b3
SourceSafeUser Lofty.mettyer <SourceSafeUser Lofty.mettyer>
2013-06-06 15:43:21 +0100

    {14/09/2011 10:31:08}First draft

---
+++ Client Apps/HR Pro System Manager/Update Scripts/HRProUpdate-5-0.sql
@@ -0,0 +1,137 @@
+/* --------------------------------------------------- */
+/* Update the database from version 4.3 to version 5.0 */
+/* --------------------------------------------------- */
+
+DECLARE @iRecCount integer,
+	@sDBVersion varchar(10),
+	@DBName varchar(255),
+	@Command varchar(max),
+	@iSQLVersion int,
+	@NVarCommand nvarchar(max),
+	@sObject sysname,
+	@sObjectType char(2),
+	@ptrval binary(16),
+	@sTableName	sysname,
+	@sIndexName	sysname,
+	@fPrimaryKey	bit;
+	
+DECLARE @ownerGUID uniqueidentifier
+DECLARE @nextid integer
+DECLARE @sSPCode nvarchar(max)
+
+DECLARE @admingroups TABLE(groupname nvarchar(255))
+
+
+/* ----------------------------------- */
+/* Avoid the (1 Row Affected) messages */
+/* ----------------------------------- */
+SET NOCOUNT ON
+SET @DBName = DB_NAME()
+
+/* ------------------------------------------------------- */
+/* Get the database version from the ASRSysSettings table. */
+/* ------------------------------------------------------- */
+
+SELECT @sDBVersion = [SettingValue] FROM ASRSysSystemSettings
+where [Section] = 'database' and [SettingKey] = 'version'
+
+/* Exit if the database is not previous or current version . */
+/* NB. We allow the script to run even if the database is the new version, as the flags set at the end of the script */
+/* may need to be run if we issue corrected versions of the applications without updating the database verion number. */
+IF (@sDBVersion <> '4.3') and (@sDBVersion <> '5.0')
+BEGIN
+	RAISERROR('The current database version is incompatible with this update script', 16, 1)
+	RETURN
+END
+
+-- Only allow script to be run on SQL2008 or above
+SELECT @iSQLVersion = convert(float,substring(@@version,charindex('-',@@version)+2,2))
+IF (@iSQLVersion < 9)
+BEGIN
+	RAISERROR('The SQL Server is incompatible with this version of OpenHR', 16, 1)
+	RETURN
+END
+
+/* ------------------------------------------------------------- */
+PRINT 'Step 1 - System procedures'
+
+	IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[spsys_setdefaultsystemsetting]') AND type in (N'P', N'PC'))
+		DROP PROCEDURE [dbo].[spsys_setdefaultsystemsetting];
+
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spsys_setdefaultsystemsetting](
+			@section AS varchar(50),
+			@settingkey AS varchar(50),
+			@settingvalue AS nvarchar(MAX))
+		AS
+		BEGIN
+			IF NOT EXISTS(SELECT [SettingValue] FROM [asrsyssystemsettings] WHERE [Section] = @section AND [SettingKey] = @settingkey)
+				INSERT ASRSysSystemSettings([Section], [SettingKey], [SettingValue]) VALUES (@section, @settingkey, @settingvalue);	
+		END';
+
+	EXEC dbo.spsys_setdefaultsystemsetting 'integration', 'payroll', 'OpenPay';
+
+
+
+
+/* ------------------------------------------------------------- */
+/* Update the database version flag in the ASRSysSettings table. */
+/* Dont Set the flag to refresh the stored procedures            */
+/* ------------------------------------------------------------- */
+PRINT 'Final Step - Updating Versions'
+
+	EXEC spsys_setsystemsetting 'database', 'version', '5.0';
+	EXEC spsys_setsystemsetting 'intranet', 'minimum version', '5.0.0';
+	EXEC spsys_setsystemsetting 'ssintranet', 'minimum version', '5.0.0';
+	EXEC spsys_setsystemsetting 'server dll', 'minimum version', '3.4.0';
+	EXEC spsys_setsystemsetting '.NET Assembly', 'minimum version', '4.2.0';
+	EXEC spsys_setsystemsetting 'outlook service', 'minimum version', '4.2.0';
+	EXEC spsys_setsystemsetting 'workflow service', 'minimum version', '4.2.0';
+	EXEC spsys_setsystemsetting 'system framework', 'version', '1.0.4268.21068';
+
+
+insert into asrsysauditaccess
+(DateTimeStamp, UserGroup, UserName, ComputerName, HRProModule, Action)
+values (getdate(),'<none>',left(system_user,50),lower(left(host_name(),30)),'System','v5.0')
+
+
+SELECT @NVarCommand = 
+	'IF EXISTS (SELECT * FROM dbo.sysobjects
+			WHERE id = object_id(N''[dbo].[sp_ASRLockCheck]'')
+			AND OBJECTPROPERTY(id, N''IsProcedure'') = 1)
+		GRANT EXECUTE ON sp_ASRLockCheck TO public'
+EXEC sp_executesql @NVarCommand
+
+
+SELECT @NVarCommand = 'USE master
+	GRANT EXECUTE ON sp_OACreate TO public
+	GRANT EXECUTE ON sp_OADestroy TO public
+	GRANT EXECUTE ON sp_OAGetErrorInfo TO public
+	GRANT EXECUTE ON sp_OAGetProperty TO public
+	GRANT EXECUTE ON sp_OAMethod TO public
+	GRANT EXECUTE ON sp_OASetProperty TO public
+	GRANT EXECUTE ON sp_OAStop TO public
+	GRANT EXECUTE ON xp_StartMail TO public
+	GRANT EXECUTE ON xp_SendMail TO public
+	GRANT EXECUTE ON xp_LoginConfig TO public
+	GRANT EXECUTE ON xp_EnumGroups TO public'
+
+SELECT @NVarCommand = 'USE ['+@DBName + ']
+GRANT VIEW DEFINITION TO public'
+EXEC sp_executesql @NVarCommand
+
+
+/* -------------------------------------------- */
+/* Set Refresh flag ? Comment out if not needed */
+/* -------------------------------------------- */
+EXEC dbo.spsys_setsystemsetting 'database', 'refreshstoredprocedures', 1;
+
+
+/* ------------------------------------- */
+/* Reapply the (1 Row Affected) messages */
+/* ------------------------------------- */
+SET NOCOUNT OFF;
+
+/* ------------------ */
+/* Display OK Message */
+/* ------------------ */
+PRINT 'Update Script Has Converted Your HR Pro Database To Use v5.0 Of OpenHR'
