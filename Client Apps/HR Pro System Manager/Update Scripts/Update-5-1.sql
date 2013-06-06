66ea7e238adf36e1fa74fb30b5228294b9785846
SourceSafeUser Lofty.mettyer <SourceSafeUser Lofty.mettyer>
2013-06-06 15:56:51 +0100

    {12/09/2012 09:44:50}Fusion integration

--- Client Apps/HR Pro System Manager/Update Scripts/Update-5-1.sql
+++ Client Apps/HR Pro System Manager/Update Scripts/Update-5-1.sql
@@ -88,8 +88,12 @@ PRINT 'Step - Deletion changes'
 PRINT 'Step - Fusion Message Bus Integration'
 /* ------------------------------------------------------------- */
 
-
-
+	IF NOT EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[fusion].[spFusionMessageSend]') AND xtype = 'P')
+		EXECUTE sp_executesql N'CREATE PROCEDURE fusion.spFusionMessageSend(@TableID integer, @RecordID integer)
+		AS
+		BEGIN
+			SET NOCOUNT ON;
+		END'
 
 
 
