9ed68dccfbe21c66c5fefe27cc425b1440950385
SourceSafeUser Lofty.mettyer <SourceSafeUser Lofty.mettyer>
2013-06-06 15:31:19 +0100

    {01/12/2010 10:29:38}First pass of HR Pro : Codename Phoenix

---
+++ Client Apps/HR Pro System Manager/Update Scripts/HRProUpdate-4-3.sql
@@ -0,0 +1,186 @@
+
+/* --------------------------------------------------- */
+/* Update the database from version 4.2 to version 4.3 */
+/* --------------------------------------------------- */
+
+DECLARE @iRecCount integer,
+	@sDBVersion varchar(10),
+	@DBName varchar(255),
+	@Command varchar(max),
+	@iSQLVersion int,
+	@NVarCommand nvarchar(max),
+	@sObject sysname,
+	@sObjectType char(2),
+	@ptrval binary(16)
+
+DECLARE @sSQL varchar(max)
+DECLARE @sSPCode nvarchar(max)
+
+/* ----------------------------------- */
+/* Avoid the (1 Row Affected) messages */
+/* ----------------------------------- */
+SET NOCOUNT ON
+SET @DBName = DB_NAME()
+
+/* ------------------------------------------------------- */
+/* Get the database version from the ASRSysSettings table. */
+/* ------------------------------------------------------- */
+
+SELECT @sDBVersion = [SettingValue] FROM ASRSysSystemSettings
+where [Section] = 'database' and [SettingKey] = 'version'
+
+/* Exit if the database is not previous or current version . */
+/* NB. We allow the script to run even if the database is the new version, as the flags set at the end of the script */
+/* may need to be run if we issue corrected versions of the applications without updating the database verion number. */
+IF (@sDBVersion <> '4.2') and (@sDBVersion <> '4.3')
+BEGIN
+	RAISERROR('The current database version is incompatible with this update script', 16, 1)
+	RETURN
+END
+
+-- Only allow script to be run on SQL2005 or above
+SELECT @iSQLVersion = convert(float,substring(@@version,charindex('-',@@version)+2,2))
+IF (@iSQLVersion <> 9 AND @iSQLVersion <> 10)
+BEGIN
+	RAISERROR('The SQL Server is incompatible with this version of HR Pro', 16, 1)
+	RETURN
+END
+
+/* ------------------------------------------------------------- */
+PRINT 'Step X - '
+
+
+
+
+/* ------------------------------------------------------------- */
+PRINT 'Step X - '
+
+	
+/* ------------------------------------------------------------- */
+/* ------------------------------------------------------------- */
+
+/*---------------------------------------------*/
+/* Ensure the required permissions are granted */
+/*---------------------------------------------*/
+DECLARE curObjects CURSOR LOCAL FAST_FORWARD FOR
+SELECT sysobjects.name, sysobjects.xtype
+FROM sysobjects
+     INNER JOIN sysusers ON sysobjects.uid = sysusers.uid
+WHERE (((sysobjects.xtype = 'p') AND (sysobjects.name LIKE 'sp_asr%' OR sysobjects.name LIKE 'spasr%'))
+    OR ((sysobjects.xtype = 'u') AND (sysobjects.name LIKE 'asrsys%'))
+    OR ((sysobjects.xtype = 'fn') AND (sysobjects.name LIKE 'udf_ASRFn%')))
+    AND (sysusers.name = 'dbo')
+--IF (@@ERROR <> 0) goto QuitWithRollback
+
+OPEN curObjects
+FETCH NEXT FROM curObjects INTO @sObject, @sObjectType
+WHILE (@@fetch_status = 0)
+BEGIN
+    IF rtrim(@sObjectType) = 'P' OR rtrim(@sObjectType) = 'FN'
+    BEGIN
+        SET @sSQL = 'GRANT EXEC ON [' + @sObject + '] TO [ASRSysGroup]'
+        EXEC(@sSQL)
+        --IF (@@ERROR <> 0) goto QuitWithRollback
+    END
+    ELSE
+    BEGIN
+        SET @sSQL = 'GRANT SELECT,INSERT,UPDATE,DELETE ON [' + @sObject + '] TO [ASRSysGroup]'
+        EXEC(@sSQL)
+        --IF (@@ERROR <> 0) goto QuitWithRollback
+    END
+
+    FETCH NEXT FROM curObjects INTO @sObject, @sObjectType
+END
+CLOSE curObjects
+DEALLOCATE curObjects
+
+/* ------------------------------------------------------------- */
+/* Update the database version flag in the ASRSysSettings table. */
+/* Dont Set the flag to refresh the stored procedures            */
+/* ------------------------------------------------------------- */
+PRINT 'Final Step - Updating Versions'
+
+delete from asrsyssystemsettings
+where [Section] = 'database' and [SettingKey] = 'version'
+insert ASRSysSystemSettings([Section], [SettingKey], [SettingValue])
+values('database', 'version', '4.3')
+
+delete from asrsyssystemsettings
+where [Section] = 'intranet' and [SettingKey] = 'minimum version'
+insert ASRSysSystemSettings([Section], [SettingKey], [SettingValue])
+values('intranet', 'minimum version', '4.3.0')
+
+delete from asrsyssystemsettings
+where [Section] = 'ssintranet' and [SettingKey] = 'minimum version'
+insert ASRSysSystemSettings([Section], [SettingKey], [SettingValue])
+values('ssintranet', 'minimum version', '4.3.0')
+
+delete from asrsyssystemsettings
+where [Section] = 'server dll' and [SettingKey] = 'minimum version'
+insert ASRSysSystemSettings([Section], [SettingKey], [SettingValue])
+values('server dll', 'minimum version', '3.4.0')
+
+delete from asrsyssystemsettings
+where [Section] = '.NET Assembly' and [SettingKey] = 'minimum version'
+insert ASRSysSystemSettings([Section], [SettingKey], [SettingValue])
+values('.NET Assembly', 'minimum version', '4.2.0')
+
+delete from asrsyssystemsettings
+where [Section] = 'outlook service' and [SettingKey] = 'minimum version'
+insert ASRSysSystemSettings([Section], [SettingKey], [SettingValue])
+values('outlook service', 'minimum version', '4.2.0')
+
+delete from asrsyssystemsettings
+where [Section] = 'workflow service' and [SettingKey] = 'minimum version'
+insert ASRSysSystemSettings([Section], [SettingKey], [SettingValue])
+values('workflow service', 'minimum version', '4.2.0')
+
+insert into asrsysauditaccess
+(DateTimeStamp, UserGroup, UserName, ComputerName, HRProModule, Action)
+values (getdate(),'<none>',left(system_user,50),lower(left(host_name(),30)),'System','v4.3')
+
+
+SELECT @NVarCommand = 
+	'IF EXISTS (SELECT * FROM dbo.sysobjects
+			WHERE id = object_id(N''[dbo].[sp_ASRLockCheck]'')
+			AND OBJECTPROPERTY(id, N''IsProcedure'') = 1)
+		GRANT EXECUTE ON sp_ASRLockCheck TO public'
+EXEC sp_executesql @NVarCommand
+
+
+SELECT @NVarCommand = 'USE master
+GRANT EXECUTE ON sp_OACreate TO public
+GRANT EXECUTE ON sp_OADestroy TO public
+GRANT EXECUTE ON sp_OAGetErrorInfo TO public
+GRANT EXECUTE ON sp_OAGetProperty TO public
+GRANT EXECUTE ON sp_OAMethod TO public
+GRANT EXECUTE ON sp_OASetProperty TO public
+GRANT EXECUTE ON sp_OAStop TO public
+GRANT EXECUTE ON xp_StartMail TO public
+GRANT EXECUTE ON xp_SendMail TO public
+GRANT EXECUTE ON xp_LoginConfig TO public
+GRANT EXECUTE ON xp_EnumGroups TO public'
+--EXEC sp_executesql @NVarCommand
+
+SELECT @NVarCommand = 'USE ['+@DBName + ']
+GRANT VIEW DEFINITION TO public'
+EXEC sp_executesql @NVarCommand
+
+
+/* -------------------------------------------- */
+/* Set Refresh flag ? Comment out if not needed */
+/* -------------------------------------------- */
+delete from asrsyssystemsettings
+where [Section] = 'database' and [SettingKey] = 'refreshstoredprocedures'
+insert ASRSysSystemSettings([Section], [SettingKey], [SettingValue])
+values('database', 'refreshstoredprocedures', 1)
+
+/* ------------------------------------- */
+/* Reapply the (1 Row Affected) messages */
+/* ------------------------------------- */
+SET NOCOUNT OFF
+
+/* ------------------ */
+/* Display OK Message */
+/* ------------------ */
+PRINT 'Update Script Has Converted Your HR Pro Database To Use v4.3 Of HR Pro'
