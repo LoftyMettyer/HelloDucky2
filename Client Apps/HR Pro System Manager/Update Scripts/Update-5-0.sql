b9bc0e67984f36ed20cf3732cded62311e684e64
SourceSafeUser Lofty.mettyer <SourceSafeUser Lofty.mettyer>
2013-06-06 15:49:39 +0100

    {12/03/2012 09:30:22}JIRA-2065 - Didn't like tables that weren't in the dbo schema

--- Client Apps/HR Pro System Manager/Update Scripts/Update-5-0.sql
+++ Client Apps/HR Pro System Manager/Update Scripts/Update-5-0.sql
@@ -82,6 +82,9 @@ PRINT 'Step 1 - System procedures'
 	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spASRDropTempObjects]') AND xtype = 'P')
 		DROP PROCEDURE [dbo].[spASRDropTempObjects];
 
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spASRUpdateStatistics]') AND xtype = 'P')
+		DROP PROCEDURE [dbo].[spASRUpdateStatistics];
+
 	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spASRGetCurrentUsersAppName]') AND xtype = 'P')
 		DROP PROCEDURE [dbo].[spASRGetCurrentUsersAppName];
 
@@ -186,6 +189,44 @@ PRINT 'Step 1 - System procedures'
 
 	END'
 
+
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spASRUpdateStatistics]
+	AS
+	BEGIN
+
+		SET NOCOUNT ON;
+
+		DECLARE @sTableName		nvarchar(255),
+				@sSchema		nvarchar(255),
+				@sVarCommand	nvarchar(MAX);
+
+		-- Checking fragmentation
+		DECLARE tables CURSOR FOR
+			SELECT sc.[Name], so.[Name]
+			FROM sys.sysobjects so
+				INNER JOIN sys.sysindexes si ON so.id = si.id
+				INNER JOIN sys.schemas sc ON so.uid  = sc.schema_id
+			WHERE so.type =''U'' AND si.indid < 2 AND si.rows > 0
+			ORDER BY sc.name, so.[Name];
+
+		-- Open the cursor
+		OPEN tables;
+
+		-- Loop through all the tables in the database running dbcc showcontig on each one
+		FETCH NEXT FROM tables INTO @sSchema, @sTableName;
+
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+			SET @sVarCommand = ''UPDATE STATISTICS ['' + @sSchema + ''].['' + @sTableName + ''] WITH FULLSCAN'';
+			EXECUTE sp_executeSQL @sVarCommand;
+			FETCH NEXT FROM tables INTO @sSchema, @sTableName;
+		END
+
+		-- Close and deallocate the cursor
+		CLOSE tables;
+		DEALLOCATE tables;
+
+	END'
 
 
 	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spASRWorkflowSubmitImmediatesAndGetSucceedingElements]
