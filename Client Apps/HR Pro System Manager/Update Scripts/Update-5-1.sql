146d4364092d80575c4e4ed591c4064ad12150bf
SourceSafeUser Lofty.mettyer <SourceSafeUser Lofty.mettyer>
2013-06-06 15:56:10 +0100

    {20/08/2012 15:54:04}Search all objects stuff

--- Client Apps/HR Pro System Manager/Update Scripts/Update-5-1.sql
+++ Client Apps/HR Pro System Manager/Update Scripts/Update-5-1.sql
@@ -862,8 +862,42 @@ PRINT 'Step - Menu & Category enhancements'
 		DROP VIEW [dbo].[ASRSysAllObjectAccess]
 	EXEC sp_executesql N'CREATE VIEW dbo.[ASRSysAllObjectAccess]
 	AS
-		SELECT 0 AS [objectType],* FROM [ASRSysCustomReportAccess];';
-	GRANT SELECT ON dbo.[ASRSysCustomReportAccess] TO [ASRSysGroup];
+		SELECT 0 AS [objectType], * FROM [ASRSysBatchJobAccess]
+		UNION
+		SELECT 1 AS [objectType], * FROM [ASRSysCrossTabAccess]
+		UNION
+		SELECT 2 AS [objectType], * FROM [ASRSysCustomReportAccess]
+		UNION
+		SELECT 3 AS [objectType], * FROM [ASRSysDataTransferAccess]
+		UNION
+		SELECT 4 AS [objectType], * FROM [ASRSysExportAccess]
+		UNION		
+		SELECT CASE [type] 
+					WHEN ''A'' THEN 5
+					WHEN ''D'' THEN 6
+					WHEN ''U'' THEN 7
+				END	AS [objectType], a.* FROM [ASRSysGlobalAccess] a
+			INNER JOIN ASRSysGlobalFunctions g ON a.ID = g.functionID	
+		UNION
+		SELECT 8 AS [objectType], * FROM [ASRSysImportAccess]
+		UNION
+		SELECT CASE m.[IsLabel] 
+					WHEN 0 THEN 9
+					WHEN 1 THEN 18
+				END	AS [objectType], a.* FROM ASRSysMailMergeAccess a
+			INNER JOIN ASRSysMailMergeName m ON a.ID = m.MailMergeID
+		UNION
+		SELECT CASE [MatchReportType] 
+				WHEN 0 THEN 14 
+				WHEN 1 THEN 23
+				WHEN 2 THEN 24 
+			END	AS [objectType], a.* FROM ASRSysMatchReportAccess a
+			INNER JOIN ASRSysMatchReportName m ON a.ID = m.MatchReportID			
+		UNION
+		SELECT 17 AS [objectType], * FROM ASRSysCalendarReportAccess
+		UNION
+		SELECT 20 AS [objectType], * FROM [ASRSysRecordProfileAccess]';
+	GRANT SELECT ON dbo.[ASRSysAllObjectAccess] TO [ASRSysGroup];
 
 
 
