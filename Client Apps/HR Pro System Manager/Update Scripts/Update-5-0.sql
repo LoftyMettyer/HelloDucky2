1e0794e0b84758ded68aa66dc341dfac9987dda0
SourceSafeUser Lofty.mettyer <SourceSafeUser Lofty.mettyer>
2013-06-06 15:50:08 +0100

    {14/03/2012 19:58:58}Added functionality to auto create the mobile workflow activated column.

--- Client Apps/HR Pro System Manager/Update Scripts/Update-5-0.sql
+++ Client Apps/HR Pro System Manager/Update Scripts/Update-5-0.sql
@@ -14,6 +14,9 @@ DECLARE @iRecCount integer,
 	@sTableName	sysname,
 	@sIndexName	sysname,
 	@fPrimaryKey	bit;
+
+DECLARE	@perstableid integer,
+		@columnid integer;
 	
 DECLARE @ownerGUID uniqueidentifier,
 	@nextid integer,
@@ -106,6 +109,14 @@ PRINT 'Step 1 - System procedures'
 	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spstat_getaudittrail]') AND xtype = 'P')
 		DROP PROCEDURE [dbo].[spstat_getaudittrail];
 
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spstat_scriptnewcolumn]') AND xtype = 'P')
+		DROP PROCEDURE [dbo].[spstat_scriptnewcolumn];
+
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spstat_setdefaultmodulesetting]') AND xtype = 'P')
+		DROP PROCEDURE [dbo].[spstat_setdefaultmodulesetting];
+
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spstat_setmodulesetting]') AND xtype = 'P')
+		DROP PROCEDURE [dbo].[spstat_setmodulesetting];
 
 
 	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spASRDropTempObjects]
@@ -226,7 +237,281 @@ PRINT 'Step 1 - System procedures'
 		CLOSE tables;
 		DEALLOCATE tables;
 
-	END'
+	END';
+
+
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spstat_setmodulesetting](
+			@modulekey AS varchar(50),
+			@parameterkey AS varchar(50),
+			@parametervalue AS nvarchar(1000),
+			@parametertype AS varchar(20))
+		AS
+		BEGIN
+			
+			IF EXISTS(SELECT [ParameterValue] FROM dbo.[ASRSysModuleSetup] WHERE [ModuleKey] = @modulekey AND [ParameterKey] = @parameterkey AND [ParameterType] = @parametertype)
+				UPDATE dbo.[ASRSysModuleSetup] SET [ParameterValue] = @parametervalue  WHERE [ModuleKey] = @modulekey AND [ParameterKey] = @parameterkey AND [ParameterType] = @parametertype;
+			ELSE
+				INSERT dbo.[ASRSysModuleSetup]([ModuleKey], [ParameterKey], [ParameterValue], [ParameterType])
+					VALUES (@modulekey, @parameterkey, @parametervalue, @parametertype);	
+		END';
+
+
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spstat_setdefaultmodulesetting](
+			@modulekey AS varchar(50),
+			@parameterkey AS varchar(50),
+			@parametervalue AS nvarchar(1000),
+			@parametertype AS varchar(20))
+		AS
+		BEGIN
+			
+			IF NOT EXISTS(SELECT [ParameterValue] FROM dbo.[ASRSysModuleSetup] WHERE [ModuleKey] = @modulekey AND [ParameterKey] = @parameterkey AND [ParameterType] = @parametertype)
+				INSERT dbo.[ASRSysModuleSetup]([ModuleKey], [ParameterKey], [ParameterValue], [ParameterType])
+					VALUES (@modulekey, @parameterkey, @parametervalue, @parametertype);	
+		END';
+
+
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE dbo.spstat_scriptnewcolumn (@columnid integer OUTPUT, @tableid integer, @columnname varchar(255)
+		, @datatype integer, @description varchar(255), @size integer, @decimals integer, @islocked bit)
+	AS
+	BEGIN
+
+		DECLARE @ssql nvarchar(MAX),
+				@tablename varchar(255),
+				@datasyntax	varchar(255);
+
+		DECLARE @spinnerMinimum integer,
+			@spinnerMaximum integer,
+			@spinnerIncrement integer,
+			@audit bit,
+			@duplicate bit,
+			@defaultvalue varchar(max),
+			@columntype integer,
+			@mandatory bit,
+			@uniquecheck bit,
+			@convertcase smallint,
+			@mask varchar(MAX),
+			@lookupTableID integer,
+			@lookupColumnID integer,
+			@controltype integer,
+			@alphaonly bit,
+			@blankIfZero bit,
+			@multiline bit,
+			@alignment smallint,
+			@calcExprID integer,
+			@gotFocusExprID integer,
+			@lostFocusExprID integer,
+			@calcTrigger smallint,
+			@readOnly bit,
+			@statusBarMessage varchar(255),
+			@errorMessage varchar(255),
+			@linkTableID integer, 
+			@Afdenabled bit, 
+			@Afdindividual integer,
+			@Afdforename integer, 
+			@Afdsurname integer,
+			@Afdinitial integer, 
+			@Afdtelephone integer, 
+			@Afdaddress integer,
+			@Afdproperty integer, 
+			@Afdstreet integer, 
+			@Afdlocality integer, 
+			@Afdtown integer, 
+			@Afdcounty integer,
+			@dfltValueExprID integer, 
+			@linkOrderID integer, 
+			@OleOnServer bit, 
+			@childUniqueCheck bit,
+			@LinkViewID integer, 
+			@DefaultDisplayWidth integer, 
+			@UniqueCheckType integer,
+			@Trimming integer, 
+			@Use1000Separator bit,
+			@LookupFilterColumnID integer, 
+			@LookupFilterValueID integer, 
+			@QAddressEnabled integer, 
+			@QAIndividual integer, 
+			@QAAddress integer, 
+			@QAProperty integer, 
+			@QAStreet integer,
+			@QALocality integer, 
+			@QATown integer, 
+			@QACounty integer, 
+			@LookupFilterOperator integer, 
+			@Embedded bit, 
+			@OLEType integer, 
+			@MaxOLESizeEnabled bit, 
+			@MaxOLESize integer,
+			@AutoUpdateLookupValues bit, 
+			@CalculateIfEmpty bit;
+
+		-- Can we safely create this column?
+		SELECT @columnid = ISNULL(columnid,0) FROM dbo.[ASRSysColumns] WHERE tableid = @tableid AND columnname = @columnname;
+		IF @columnid > 0
+		BEGIN
+			RETURN;
+		END
+
+		SELECT @tablename = [tablename] FROM dbo.[ASRSysTables] WHERE tableid = @tableid;
+		SELECT @columnid = MAX(columnid) + 1 FROM dbo.[ASRSysColumns];
+			
+		SET @defaultvalue = '''';		
+		SET @spinnerMinimum = 0;
+		SET @spinnerMaximum = 0;
+		SET @spinnerIncrement = 0;
+		SET @audit = 0;
+		SET @duplicate = 0;
+		SET @columntype = 0;
+		SET @mandatory = 0;
+		SET @uniquecheck = 0;
+		SET @convertcase = 0;
+		SET @mask = '''';
+		SET @lookupTableID = 0;
+		SET	@lookupColumnID = 0;
+		SET	@controltype = 0;	
+		SET @alphaonly = 0;
+		SET @blankIfZero = 0;
+		SET @multiline = 0;
+		SET @alignment = 0;
+		SET @calcExprID = 0;
+		SET @gotFocusExprID = 0;
+		SET @lostFocusExprID = 0;
+		SET @calcTrigger = 0;
+		SET @readOnly = 0;
+		SET @statusBarMessage = '''';
+		SET @errorMessage = '''';
+		SET @linkTableID = 0; 
+		SET @Afdenabled = 0; 
+		SET @Afdindividual = 0;
+		SET @Afdforename = 0; 
+		SET @Afdsurname = 0;
+		SET @Afdinitial = 0; 
+		SET @Afdtelephone = 0; 
+		SET @Afdaddress = 0;
+		SET @Afdproperty = 0; 
+		SET @Afdstreet = 0; 
+		SET @Afdlocality = 0; 
+		SET @Afdtown = 0; 
+		SET @Afdcounty = 0;
+		SET @dfltValueExprID = 0; 
+		SET @linkOrderID = 0; 
+		SET @OleOnServer = 0; 
+		SET @childUniqueCheck = 0;
+		SET @LinkViewID = 0; 
+		SET @DefaultDisplayWidth = 0; 
+		SET @UniqueCheckType = 0;
+		SET @Trimming = 0;
+		SET @Use1000Separator = 0;
+		SET @LookupFilterColumnID = 0; 
+		SET @LookupFilterValueID = 0; 
+		SET @QAddressEnabled = 0; 
+		SET @QAIndividual = 0; 
+		SET @QAAddress = 0; 
+		SET @QAProperty = 0; 
+		SET @QAStreet = 0;
+		SET @QALocality = 0; 
+		SET @QATown = 0; 
+		SET @QACounty = 0; 
+		SET @LookupFilterOperator = 0; 
+		SET @Embedded = 0; 
+		SET @OLEType = 0; 
+		SET @MaxOLESizeEnabled = 0; 
+		SET @MaxOLESize = 0;
+		SET @AutoUpdateLookupValues = 0; 
+		SET @CalculateIfEmpty = 0;
+		
+
+		-- Logic
+		IF @datatype = -7
+		BEGIN
+			SET @datasyntax = ''bit'';
+			SET @defaultvalue = ''FALSE'';
+			SET @controltype = 1;
+		END
+
+		-- OLE
+		IF @datatype = -4
+			SET @controltype = 1;
+
+		-- Photo
+		IF @datatype = -3
+			SET @controltype = 1024;
+
+		-- Link
+		IF @datatype = -2
+		BEGIN
+			SET @datasyntax = ''varchar(255)'';
+			SET @controltype = 2048;
+		END
+
+		-- Working Pattern
+		IF @datatype = -1
+		BEGIN
+			SET @datasyntax = ''varchar(14)'';
+			SET @controltype = 4096;
+		END
+		
+		-- Numeric
+		IF @datatype = 2
+		BEGIN
+			SET @datasyntax = ''numeric('' + @size + '','' + @decimals + '')'';
+			SET @defaultvalue = 0;	
+			SET @controltype = 64;
+		END
+
+		-- Integers
+		IF @datatype = 4
+		BEGIN
+			SET @datasyntax = ''integer'';
+			SET @controltype = 64;
+		END
+		
+		-- Date
+		IF @datatype = 11
+		BEGIN
+			SET @datasyntax = ''datetime'';
+			SET @controltype = 64;
+		END
+
+		-- Character
+		IF @datatype = 12
+		BEGIN
+			SET @datasyntax = ''varchar('' + @size + '')'';
+			SET @controltype = 64;
+		END
+
+		-- System objects update
+		INSERT dbo.[tbsys_scriptedobjects] ([guid], [objecttype], [targetid], [ownerid], [effectivedate], [revision], [locked], [lastupdated])
+			SELECT NEWID(), 2, @columnid, ''AAAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE'', ''01/01/1900'',1,@islocked, GETDATE()
+
+		-- Update base table								
+		INSERT dbo.[tbsys_columns] ([columnID], [tableID], [columnType], [datatype], [defaultValue], [size], [decimals]
+				, [lookupTableID], [lookupColumnID], [controltype], [spinnerMinimum], [spinnerMaximum], [spinnerIncrement], [audit]
+				, [duplicate], [mandatory], [uniquecheck], [convertcase], [mask], [alphaonly], [blankIfZero], [multiline], [alignment]
+				, [calcExprID], [gotFocusExprID], [lostFocusExprID], [calcTrigger], [readOnly], [statusBarMessage], [errorMessage]
+				, [linkTableID], [Afdenabled], [Afdindividual], [Afdforename], [Afdsurname], [Afdinitial], [Afdtelephone], [Afdaddress]
+				, [Afdproperty], [Afdstreet], [Afdlocality], [Afdtown], [Afdcounty], [dfltValueExprID], [linkOrderID], [OleOnServer]
+				, [childUniqueCheck], [LinkViewID], [DefaultDisplayWidth], [ColumnName], [UniqueCheckType], [Trimming], [Use1000Separator]
+				, [LookupFilterColumnID], [LookupFilterValueID], [QAddressEnabled], [QAIndividual], [QAAddress], [QAProperty], [QAStreet]
+				, [QALocality], [QATown], [QACounty], [LookupFilterOperator], [Embedded], [OLEType], [MaxOLESizeEnabled], [MaxOLESize]
+				, [AutoUpdateLookupValues], [CalculateIfEmpty]) 
+			VALUES (@columnid, @tableid, @columntype, @datatype, @defaultvalue, @size, @decimals
+				, @lookupTableID, @lookupColumnID, @controltype, @spinnerMinimum, @spinnerMaximum, @spinnerIncrement, @audit
+				, @duplicate, @mandatory, @uniquecheck, @convertcase, @mask, @alphaonly, @blankIfZero, @multiline, @alignment
+				, @calcExprID, @gotFocusExprID, @lostFocusExprID, @calcTrigger, @readOnly, @statusBarMessage, @errorMessage
+				, @linkTableID, @Afdenabled, @Afdindividual, @Afdforename, @Afdsurname, @Afdinitial, @Afdtelephone, @Afdaddress
+				, @Afdproperty, @Afdstreet, @Afdlocality, @Afdtown, @Afdcounty, @dfltValueExprID, @linkOrderID, @OleOnServer
+				, @childUniqueCheck, @LinkViewID, @DefaultDisplayWidth, @ColumnName, @UniqueCheckType, @Trimming, @Use1000Separator
+				, @LookupFilterColumnID, @LookupFilterValueID, @QAddressEnabled, @QAIndividual, @QAAddress, @QAProperty, @QAStreet
+				, @QALocality, @QATown, @QACounty, @LookupFilterOperator, @Embedded, @OLEType, @MaxOLESizeEnabled, @MaxOLESize
+				, @AutoUpdateLookupValues, @CalculateIfEmpty);
+
+			-- Physically create this column (is regenerated by the System Manager save)	
+			SET @ssql = N''ALTER TABLE dbo.tbuser_'' + @tablename + '' ADD '' + @columnname + '' '' + @datasyntax;
+			EXECUTE sp_executesql @ssql;
+
+		RETURN;
+
+	END';
 
 
 	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spASRWorkflowSubmitImmediatesAndGetSucceedingElements]
@@ -4500,12 +4785,6 @@ PRINT 'Step 10 - Message Bus Integration'
 			[LastProcessedDate] [datetime] NULL,
 			[LastGeneratedXml] [varchar](max) NULL);';
 
-
-
-
-	DECLARE	@perstableid integer,
-			@columnid integer;
-
 	-- Message table mapping defintion
 	IF NOT EXISTS(SELECT * FROM sys.sysobjects where name = 'ASRSysFusionTypes' AND xtype = 'U')
 	BEGIN
@@ -5071,6 +5350,35 @@ PRINT 'Step 11 - System Calculations'
 
 	UPDATE dbo.[tbstat_componentcode] SET [recordidrequired] = 1 WHERE [ID] = 43 AND [isoperator] = 0;
 
+
+
+/* ------------------------------------------------------------- */
+/* Attempts to default some new configuration options.           */
+/* ------------------------------------------------------------- */
+PRINT 'Step 12 - Auto Configuration'
+
+	SELECT @perstableid = ISNULL([parametervalue],0) FROM dbo.[ASRSysModuleSetup]
+		WHERE ModuleKey = 'MODULE_PERSONNEL' AND ParameterKey = 'Param_TablePersonnel' AND [ParameterType] = 'PType_TableID';
+
+	IF @perstableid IS NOT NULL
+	BEGIN
+
+		EXECUTE dbo.spstat_setdefaultmodulesetting 'MODULE_MOBILE', 'Param_TablePersonnel', @perstableid, 'PType_TableID';
+
+		SELECT @columnid = ISNULL([parametervalue],0) FROM dbo.[ASRSysModuleSetup] WHERE ModuleKey = 'MODULE_PERSONNEL' AND ParameterKey = 'Param_FieldsLeavingDate' AND [ParameterType] = 'PType_ColumnID';
+		EXECUTE dbo.spstat_setdefaultmodulesetting 'MODULE_MOBILE', 'Param_FieldsLeavingDate', @columnid, 'PType_ColumnID';
+
+		SET @columnid = 0;
+		EXECUTE dbo.spstat_scriptnewcolumn @columnid OUTPUT, @perstableid, 'Mobile_Workflow_Activated', -7, 'Checkbox if the workflow mobile is activated for this user', 1, 0, 1;
+		EXECUTE dbo.spstat_setdefaultmodulesetting 'MODULE_MOBILE', 'Param_MobileActivated', @columnid, 'PType_ColumnID';
+
+		SELECT @columnid = ISNULL([parametervalue],0) FROM dbo.[ASRSysModuleSetup] WHERE ModuleKey = 'MODULE_PERSONNEL' AND ParameterKey = 'Param_FieldsEmail' AND [ParameterType] = 'PType_ColumnID';
+		EXECUTE dbo.spstat_setdefaultmodulesetting 'MODULE_MOBILE', 'Param_UniqueEmailColumn', @columnid, 'PType_ColumnID';
+
+		SELECT @columnid = ISNULL([parametervalue],0) FROM dbo.[ASRSysModuleSetup] WHERE ModuleKey = 'MODULE_PERSONNEL' AND ParameterKey = 'Param_FieldsLoginName' AND [ParameterType] = 'PType_ColumnID';
+		EXECUTE dbo.spstat_setdefaultmodulesetting 'MODULE_MOBILE', 'Param_FieldsLoginName', @columnid, 'PType_ColumnID';
+
+	END
 
 
 /* ------------------------------------------------------------- */
