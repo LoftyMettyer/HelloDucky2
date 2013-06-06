20f4ef03e2448a6b6762ba68a935084344493006
SourceSafeUser Lofty.mettyer <SourceSafeUser Lofty.mettyer>
2013-06-06 15:13:03 +0100

    {22/02/2010 12:54:46}Added udf overlap function for overlapping dates

--- Client Apps/HR Pro System Manager/Update Scripts/HRProUpdate-4-1.sql
+++ Client Apps/HR Pro System Manager/Update Scripts/HRProUpdate-4-1.sql
@@ -1194,6 +1194,68 @@ PRINT 'Step 4 - Overlapping dates functionality'
 	END
 
 
+	IF EXISTS (SELECT *
+		FROM dbo.sysobjects
+		WHERE id = object_id(N'[dbo].[udfDateOverlap]')
+			AND OBJECTPROPERTY(id, N'IsScalarFunction ') = 1)
+		DROP FUNCTION [dbo].[udfDateOverlap];
+
+	SET @sSPCode = 'CREATE FUNCTION dbo.[udfDateOverlap]()
+		RETURNS bit
+		AS
+		BEGIN
+			DECLARE @bDummy bit;
+			RETURN @bDummy;
+		END';
+	EXECUTE sp_executeSQL @sSPCode;
+
+
+	SET @sSPCode = 'ALTER FUNCTION dbo.[udfDateOverlap](
+		@pdStartDate1		datetime,
+		@psStartSession1	nvarchar(2),
+		@pdEndDate1			datetime,
+		@psEndSession1		nvarchar(2),
+		@psType1			nvarchar(MAX),
+		@pdStartDate2		datetime,
+		@psStartSession2	nvarchar(2),
+		@pdEndDate2			datetime,
+		@psEndSession2		nvarchar(2),
+		@psType2			nvarchar(MAX))
+	RETURNS bit
+	AS
+	BEGIN
+		
+		DECLARE @bFound bit;
+		SET @bFound = 0;
+		
+		-- 1st of data is the inserted, 2nd is physical database values.	
+		SET @pdStartDate1 = DATEADD(D, 0, DATEDIFF(D, 0, @pdStartDate1));
+		SET @pdEndDate1 = ISNULL(DATEADD(D, 0, DATEDIFF(D, 0, @pdEndDate1)), CONVERT(datetime,''9999-12-31''));
+		SET @pdStartDate2 = DATEADD(D, 0, DATEDIFF(D, 0, @pdStartDate2));
+		SET @pdEndDate2 = ISNULL(DATEADD(D, 0, DATEDIFF(D, 0, @pdEndDate2)), CONVERT(datetime,''9999-12-31''));
+
+		-- Put the AM/PM stuff into the above dates.
+		IF @psStartSession1 = ''PM'' SET @pdStartDate1 = DATEADD(hh, 12, @pdStartDate1);
+		IF @psEndSession1 = ''PM'' SET @pdEndDate1 = DATEADD(hh, 23, @pdEndDate1);
+		IF @psStartSession2 = ''PM'' SET @pdStartDate2 = DATEADD(hh, 12, @pdStartDate2);
+		IF @psEndSession2 = ''PM'' SET @pdEndDate2 = DATEADD(hh, 23, @pdEndDate2);
+
+		-- Check to see if this date overlaps.
+		IF ((@pdStartDate1 BETWEEN @pdStartDate2 AND @pdEndDate2)
+			OR (@pdEndDate1 BETWEEN @pdStartDate2 AND @pdEndDate2)
+			OR (@pdStartDate1 < @pdStartDate2 AND @pdEndDate1 > @pdEndDate2))
+			AND (@psType1 = @psType2 OR @psType1 IS NULL)
+				SET @bFound = 1;
+
+		RETURN @bFound;
+
+	END'
+	EXECUTE sp_executeSQL @sSPCode;
+
+
+
+
+
 /* ------------------------------------------------------------- */
 PRINT 'Step 5 - Intranet Dashboard Implementation'
 
