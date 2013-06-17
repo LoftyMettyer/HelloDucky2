472bb24cd95622657cd550738cffb58ea5adf739
SourceSafeUser Harpdev01 <SourceSafeUser Harpdev01>
2013-06-17 15:41:24 +0100

    {10/02/2011 11:52:28~10/02/2011 11:52:42}

--- Workflow/HR Pro Workflow Website/HRProWorkflow_deploy/AssemblyInfo/Release/AssemblyInfo.cs
+++ Workflow/HR Pro Workflow Website/HRProWorkflow_deploy/AssemblyInfo/Release/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyFileVersion("4.2.0.2")]
-[assembly: AssemblyVersion("4.2.0.2")]
+[assembly: AssemblyFileVersion("4.2.0.12")]
+[assembly: AssemblyVersion("4.2.0.12")]
