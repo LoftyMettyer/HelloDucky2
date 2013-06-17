79d18836871107905ee31049bb63403b9ced9486
SourceSafeUser Harpdev01 <SourceSafeUser Harpdev01>
2013-06-17 15:46:41 +0100

    {01/05/2012 17:32:06~01/05/2012 17:32:16}

--- Workflow/OpenHR Workflow Website/OpenHRWorkflow_deploy/AssemblyInfo/Release/AssemblyInfo.cs
+++ Workflow/OpenHR Workflow Website/OpenHRWorkflow_deploy/AssemblyInfo/Release/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-[assembly: AssemblyFileVersion("5.0.12.0")]
-[assembly: AssemblyVersion("5.0.12.0")]
+[assembly: AssemblyFileVersion("5.0.13.0")]
+[assembly: AssemblyVersion("5.0.13.0")]
