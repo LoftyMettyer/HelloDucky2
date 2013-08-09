351047776ce9ce31a644a280d4938cf8f034d349
Roberto Caballero <COA\roberto.caballero>
2013-08-09 11:38:11 +0100

    Tabify BundleConfig.vb

--- Web Apps/DMI.NET/DMI.NET/App_Start/BundleConfig.vb
+++ Web Apps/DMI.NET/DMI.NET/App_Start/BundleConfig.vb
@@ -2,17 +2,17 @@
 
 Namespace App_Start
 
-  Public Class BundleConfig
+	Public Class BundleConfig
 
-    Public Shared Sub RegisterBundles(bundles As BundleCollection)
-      
-      ' Microsoft elements
-      Dim codeBundle As New ScriptBundle("~/bundles/Microsoft")
-      codeBundle.IncludeDirectory("~/Scripts/Microsoft", "*.js")
-      bundles.Add(codeBundle)
+		Public Shared Sub RegisterBundles(bundles As BundleCollection)
+
+			' Microsoft elements
+			Dim codeBundle As New ScriptBundle("~/bundles/Microsoft")
+			codeBundle.IncludeDirectory("~/Scripts/Microsoft", "*.js")
+			bundles.Add(codeBundle)
 
 
-      ' JQuery core
+			' JQuery core
 			bundles.Add(New ScriptBundle("~/bundles/jQuery").Include(
 					"~/Scripts/jquery/jquery-{version}.js",
 					"~/Scripts/jquery/jquery-cookie.js",
@@ -24,22 +24,22 @@ Namespace App_Start
 					"~/Scripts/date.js"))
 
 
-      ' JQuery UI
-      bundles.Add(New ScriptBundle("~/bundles/jQueryUI7").Include(
-        "~/Scripts/jquery/jquery-{version}.js",
-        "~/Scripts/jquery/jquery-ui-{version}.custom.js",
-        "~/Scripts/jquery/jquery.jqGrid.src.js",
-        "~/Scripts/jquery/jsTree/jquery.jstree.js",
-        "~/Scripts/jquery/jquery.gridster.js",
-        "~/Scripts/jquery/jquery.menu.js",
-        "~/Scripts/jquery/jquery.marquee.js",
-        "~/Scripts/jquery/jquery.maskedinput.js",
-        "~/Scripts/jquery/jquery.mousewheel.js",
-        "~/Scripts/jquery/jquery.rightClick.js",
-        "~/Scripts/jquery/jquery.ui.touch-punch.min.js",
-        "~/Scripts/officebar/jquery.officebar.js"))
+			' JQuery UI
+			bundles.Add(New ScriptBundle("~/bundles/jQueryUI7").Include(
+				"~/Scripts/jquery/jquery-{version}.js",
+				"~/Scripts/jquery/jquery-ui-{version}.custom.js",
+				"~/Scripts/jquery/jquery.jqGrid.src.js",
+				"~/Scripts/jquery/jsTree/jquery.jstree.js",
+				"~/Scripts/jquery/jquery.gridster.js",
+				"~/Scripts/jquery/jquery.menu.js",
+				"~/Scripts/jquery/jquery.marquee.js",
+				"~/Scripts/jquery/jquery.maskedinput.js",
+				"~/Scripts/jquery/jquery.mousewheel.js",
+				"~/Scripts/jquery/jquery.rightClick.js",
+				"~/Scripts/jquery/jquery.ui.touch-punch.min.js",
+				"~/Scripts/officebar/jquery.officebar.js"))
 
-      ' OpenHR core
+			' OpenHR core
 			bundles.Add(New ScriptBundle("~/bundles/OpenHR_General").Include(
 				"~/Scripts/openHR.js",
 				"~/Scripts/clientDLL.js",
@@ -48,41 +48,41 @@ Namespace App_Start
 				"~/Scripts/ctl_SetFont.js",
 				"~/Scripts/ctl_SetStyles.js"))
 
-      ' Custom Reports
-      bundles.Add(New ScriptBundle("~/bundles/utilities_customreports").Include(
-        "~/Scripts/FormScripts/customreport.js",
-        "~/Scripts/FormScripts/util_def_customreports.js",
-        "~/Scripts/FormScripts/general.js"))
+			' Custom Reports
+			bundles.Add(New ScriptBundle("~/bundles/utilities_customreports").Include(
+				"~/Scripts/FormScripts/customreport.js",
+				"~/Scripts/FormScripts/util_def_customreports.js",
+				"~/Scripts/FormScripts/general.js"))
 
-      ' Cross Tabs
-      bundles.Add(New ScriptBundle("~/bundles/utilities_crosstabs").Include(
-        "~/Scripts/FormScripts/crosstab.js",
-        "~/Scripts/FormScripts/crosstabdef.js",
-        "~/Scripts/FormScripts/general.js"))
+			' Cross Tabs
+			bundles.Add(New ScriptBundle("~/bundles/utilities_crosstabs").Include(
+				"~/Scripts/FormScripts/crosstab.js",
+				"~/Scripts/FormScripts/crosstabdef.js",
+				"~/Scripts/FormScripts/general.js"))
 
-      ' Calendar Reports
-      bundles.Add(New ScriptBundle("~/bundles/utilities_calendarreports").Include(
-        "~/Scripts/FormScripts/calendarreportdef.js",
-        "~/Scripts/FormScripts/general.js"))
+			' Calendar Reports
+			bundles.Add(New ScriptBundle("~/bundles/utilities_calendarreports").Include(
+				"~/Scripts/FormScripts/calendarreportdef.js",
+				"~/Scripts/FormScripts/general.js"))
 
-      ' Calendar Reports (run)
-      bundles.Add(New ScriptBundle("~/bundles/utilities_calendarreport_run").Include(
-        "~/Scripts/FormScripts/util_run_calendarreports.js"))
+			' Calendar Reports (run)
+			bundles.Add(New ScriptBundle("~/bundles/utilities_calendarreport_run").Include(
+				"~/Scripts/FormScripts/util_run_calendarreports.js"))
 
-      ' Standard Reports
-      bundles.Add(New ScriptBundle("~/bundles/utilities_standardreports").Include(
-        "~/Scripts/FormScripts/stdrpt_def_absence.js"))
+			' Standard Reports
+			bundles.Add(New ScriptBundle("~/bundles/utilities_standardreports").Include(
+				"~/Scripts/FormScripts/stdrpt_def_absence.js"))
 
-      ' Expression Builder
-      bundles.Add(New ScriptBundle("~/bundles/utilities_expressions").Include(
-        "~/Scripts/FormScripts/util_def_expression.js",
-        "~/Scripts/FormScripts/util_def_exprcomponent.js"))
+			' Expression Builder
+			bundles.Add(New ScriptBundle("~/bundles/utilities_expressions").Include(
+				"~/Scripts/FormScripts/util_def_expression.js",
+				"~/Scripts/FormScripts/util_def_exprcomponent.js"))
 
-      ' Mail Merge
-      bundles.Add(New ScriptBundle("~/bundles/utilities_mailmerge").Include(
-        "~/Scripts/FormScripts/util_def_mailmerge.js"))
+			' Mail Merge
+			bundles.Add(New ScriptBundle("~/bundles/utilities_mailmerge").Include(
+				"~/Scripts/FormScripts/util_def_mailmerge.js"))
 
-      ' Record Editing
+			' Record Editing
 			bundles.Add(New ScriptBundle("~/bundles/recordedit").Include(
 				"~/Scripts/jquery/jquery.jqGrid.src.js",
 				"~/Scripts/FormScripts/find.js",
@@ -91,12 +91,12 @@ Namespace App_Start
 
 #If DEBUG Then
 
-      For Each bundle As Bundle In BundleTable.Bundles
-        bundle.Transforms.Clear()
-      Next
+			For Each bundle As Bundle In BundleTable.Bundles
+				bundle.Transforms.Clear()
+			Next
 
 #End If
 
-    End Sub
-  End Class
+		End Sub
+	End Class
 End Namespace
