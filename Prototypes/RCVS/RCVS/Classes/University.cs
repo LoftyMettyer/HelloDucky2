7ca3c158281c82fe4c37faf013406151298a15d5
Lofty Mettyer <COA\lofty.mettyer>
2013-08-20 16:06:57 +0100

    Added class folder

---
+++ Prototypes/RCVS/RCVS/Classes/University.cs
@@ -0,0 +1,9 @@
+﻿namespace RCVS.Classes
+{
+	public class University
+	{
+		public string Name { get; set; }
+		public string City { get; set; }
+		public string Country { get; set; }
+	}
+}
---
+++ Prototypes/RCVS/RCVS/Models/Form1A2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RCVS.Models
+{
+	public class Form1A2
+	{
+		[Required]
+		[DisplayName("YEAR in which you plan to sit the statutory membership examination")]
+		public int YearToSit { get; set; }
+
+		public string Footnote1
+		{
+			get { return "If you do not sit this examination at the next available session or, if you must re-confirm your intention to sit within a reasonable period of time by completing a renewal of intention form"; }
+		}
+
+		[DisplayName("Do you plan to 'see practice'?")]
+		public bool? PlanToSeePractice { get; set; }
+
+		[DisplayName("Are you currently seeing practice or have you made arrangements?")]
+		public bool? CurrentlySeeingPractice { get; set; }
+
+		public string IELTS { get; set; }
+
+		public DateTime? TakeTestPlanDate { get; set; }
+
+//		public TRF
+
+
+		public Classes.University UniversityAwarded { get; set; }
+
+
+
+	}
+}
