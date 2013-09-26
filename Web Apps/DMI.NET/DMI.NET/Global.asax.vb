90f76e229881520abea00c5a04f9791c5953aa4a
Roberto Caballero <COA\roberto.caballero>
2013-09-26 14:23:04 +0100

    Changed version shown in Login screen from 8.0.12 to 8.0.13

--- Web Apps/DMI.NET/DMI.NET/Global.asax.vb
+++ Web Apps/DMI.NET/DMI.NET/Global.asax.vb
@@ -44,7 +44,7 @@ Public Class MvcApplication
 		Dim sBrowserInfo As String
 		Dim iIEVersion As Integer
 
-		Session("version") = "8.0.12"
+		Session("version") = "8.0.13"
 		Session.Timeout = 20
 		Session("TimeoutSecs") = Session.Timeout * 60
 		Server.ScriptTimeout = 1000
