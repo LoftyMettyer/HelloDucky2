0a59577e0ed69959b53f590d6b4d60c76c35d150
Roberto Caballero <COA\roberto.caballero>
2013-11-29 16:45:02 +0000

    Task 2994: Deployment Task for Build 19

--- Web Apps/DMI.NET/DMI.NET/Global.asax.vb
+++ Web Apps/DMI.NET/DMI.NET/Global.asax.vb
@@ -44,7 +44,7 @@ Public Class MvcApplication
 		Dim sBrowserInfo As String
 		Dim iIEVersion As Integer
 
-		Session("version") = "8.0.18"
+		Session("version") = "8.0.19"
 		Session.Timeout = 20
 		Session("TimeoutSecs") = Session.Timeout * 60
 		Server.ScriptTimeout = 1000
