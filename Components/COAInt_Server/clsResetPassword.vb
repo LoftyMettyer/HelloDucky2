bbfe425151ba3255e433cb7401242de9e5faa193
Lofty Mettyer <COA\lofty.mettyer>
2014-01-11 18:58:48 +0000

    Converted the reset password over to ado.net
    There are also two added stored prcoedures - these are auto generated by the system manager, but we need to include them in our db project.

--- Components/COAInt_Server/clsResetPassword.vb
+++ Components/COAInt_Server/clsResetPassword.vb
@@ -1,93 +1,60 @@
-Option Strict Off
+Option Strict On
 Option Explicit On
-Imports ADODB
+
+Imports System.Data.SqlClient
+Imports HR.Intranet.Server.Structures
 
 Public Class clsResetPassword
-	Public gsDatabase As String
-	Public gsServerName As String
-	Public gsUsername As String
-	Public mADOCon As Connection
 
+	Private ReadOnly _db As clsDataAccess
+	Private ReadOnly _objLogin As New LoginInfo
 
-	' We don't use gADOCon in this module as we're obviously not logged in when resetting a password.
-	' so, run the sp to count users on server using fixed credentials.
+	Public Sub New()
 
-	Public Function GetCurrentUsersCountOnServer() As Integer
-		mADOCon = New Connection
-		Dim cmdCheckUserSessions As New Command
+		MyBase.New()
+		_objLogin.Username = fixedUsername
+		_objLogin.Password = fixedPassword
+		_db = New clsDataAccess(_objLogin)
 
-		mADOCon.ConnectionString = ConnectionString()
-		mADOCon.Open()
+	End Sub
 
-		cmdCheckUserSessions.CommandText = "spASRGetCurrentUsersCountOnServer"
-		cmdCheckUserSessions.CommandType = CommandTypeEnum.adCmdStoredProc
-
-		Dim prmCount As Parameter = cmdCheckUserSessions.CreateParameter("count", DataTypeEnum.adInteger, ParameterDirectionEnum.adParamOutput)
-		cmdCheckUserSessions.Parameters.Append(prmCount)
-
-		Dim prmUserName As Parameter = cmdCheckUserSessions.CreateParameter("userName", DataTypeEnum.adVarChar, ParameterDirectionEnum.adParamInput, 255)
-		cmdCheckUserSessions.Parameters.Append(prmUserName)
-		prmUserName.Value = gsUsername
-
-		Err.Clear()
-
-		cmdCheckUserSessions.ActiveConnection = mADOCon
-		cmdCheckUserSessions.Execute()
-
-		Return CInt(cmdCheckUserSessions.Parameters("count").Value)
-	End Function
 	Public Function GenerateLinkAndEmail(WebsiteURL As String, Timestamp As Date) As String
 		Dim objCrypt As New clsCrypt
 		Dim sEncryptedString As String
-		Dim cmdResetPassword As Command
 		Dim skey As String
 		Dim sSourceString As String
 
 		Const ENCRYPTIONKEY As String = "jmltn"
 
-		skey = ENCRYPTIONKEY
+		Try
 
-		sSourceString = Username & vbTab & CStr(Timestamp) & vbTab & ServerName & vbTab & Database
+			skey = ENCRYPTIONKEY
 
-		sEncryptedString = objCrypt.EncryptString(sSourceString, skey, True)
-		sEncryptedString = objCrypt.CompactString(sEncryptedString)
+			sSourceString = Username & vbTab & CStr(Timestamp) & vbTab & ServerName & vbTab & Database
 
-		' Got the string, now send it to SQL with the timestamp and WebsiteURL
-		' timestamp is used to ensure old links aren't reused
-		' WebsiteURL used to generate the e-mail link to the resetpassword page.
+			sEncryptedString = objCrypt.EncryptString(sSourceString, skey, True)
+			sEncryptedString = objCrypt.CompactString(sEncryptedString)
 
-		mADOCon = New Connection
-		cmdResetPassword = New Command
+			' Got the string, now send it to SQL with the timestamp and WebsiteURL
+			' timestamp is used to ensure old links aren't reused
+			' WebsiteURL used to generate the e-mail link to the resetpassword page.
 
-		mADOCon.ConnectionString = ConnectionString()
-		mADOCon.Open()
+			Dim prmMessage As New SqlParameter("psMessage", SqlDbType.VarChar, -1) With {.Direction = ParameterDirection.InputOutput, .Value = ""}
 
-		cmdResetPassword.CommandText = "spadmin_resetpassword"
-		cmdResetPassword.CommandType = CommandTypeEnum.adCmdStoredProc
+			_db.ExecuteSP("spadmin_resetpassword", _
+					New SqlParameter("psWebsiteURL", SqlDbType.VarChar, 255) With {.Value = WebsiteURL}, _
+					New SqlParameter("psUserName", SqlDbType.VarChar, 255) With {.Value = gsUsername}, _
+					New SqlParameter("psEncryptedLink", SqlDbType.VarChar, -1) With {.Value = sEncryptedString}, _
+					prmMessage)
 
-		Dim prmWebsiteURL As Parameter = cmdResetPassword.CreateParameter("WebsiteURL", ADODB.DataTypeEnum.adVarChar, ADODB.ParameterDirectionEnum.adParamInput, 255)
-		cmdResetPassword.Parameters.Append(prmWebsiteURL)
-		prmWebsiteURL.Value = WebsiteURL
+			Return prmMessage.Value.ToString()
+			' E-mail has been sent. When you receive your sign in information, follow the directions in the email to reset your password.
 
-		Dim prmUserName As Parameter = cmdResetPassword.CreateParameter("userName", ADODB.DataTypeEnum.adVarChar, ADODB.ParameterDirectionEnum.adParamInput, 255)
-		cmdResetPassword.Parameters.Append(prmUserName)
-		prmUserName.Value = gsUsername
+		Catch ex As Exception
+			Throw
 
-		Dim prmEncryptedLink As Parameter = cmdResetPassword.CreateParameter("encryptedLink", ADODB.DataTypeEnum.adVarChar, ADODB.ParameterDirectionEnum.adParamInput, 2147483646)
-		cmdResetPassword.Parameters.Append(prmEncryptedLink)
-		prmEncryptedLink.Value = sEncryptedString
+		End Try
 
-		Dim prmResult As Parameter = cmdResetPassword.CreateParameter("message", ADODB.DataTypeEnum.adVarChar, ADODB.ParameterDirectionEnum.adParamOutput, 2147483646)
-		cmdResetPassword.Parameters.Append(prmResult)
-
-		Err.Clear()
-
-		cmdResetPassword.ActiveConnection = mADOCon
-
-		cmdResetPassword.Execute()
-
-		Return CStr(cmdResetPassword.Parameters("message").Value)
-		' E-mail has been sent. When you receive your sign in information, follow the directions in the email to reset your password.
 	End Function
 
 	Public Function GetUsernameFromQueryString(QueryString As String) As String
@@ -133,44 +100,20 @@ GetUsernameFromQueryString_ERROR:
 
 	Public Function ResetPassword(psEncryptedKey As String, psNewPassword As String) As String
 
-		Dim cmdResetPassword As Command
-		' using sp_password cos everything else does. When this sp is removed,
-		' we can then move everything up to the replacement together.
+		Try
+			Dim prmMessage As New SqlParameter("ErrorMessage", SqlDbType.VarChar, 4000) With {.Direction = ParameterDirection.Output}
 
-		mADOCon = New Connection
-		cmdResetPassword = New Command
+			_db.ExecuteSP("spadmin_commitresetpassword", _
+					New SqlParameter("code", SqlDbType.NVarChar, 4000) With {.Value = psEncryptedKey}, _
+					New SqlParameter("NewPassword", SqlDbType.NVarChar, 4000) With {.Value = psNewPassword}, _
+					prmMessage)
 
-		mADOCon.ConnectionString = ConnectionString()
-		mADOCon.Open()
+			Return prmMessage.Value.ToString()
 
-		cmdResetPassword.CommandText = "spadmin_commitresetpassword"
-		cmdResetPassword.CommandType = CommandTypeEnum.adCmdStoredProc
+		Catch ex As Exception
+			Return ""
 
-		Dim prmEncryptedKey As Parameter = cmdResetPassword.CreateParameter("EncryptedKey", ADODB.DataTypeEnum.adVarChar, ADODB.ParameterDirectionEnum.adParamInput, 4000)
-		cmdResetPassword.Parameters.Append(prmEncryptedKey)
-		prmEncryptedKey.Value = psEncryptedKey
-
-		Dim prmNewPwd As Parameter = cmdResetPassword.CreateParameter("newPassword", ADODB.DataTypeEnum.adVarChar, ADODB.ParameterDirectionEnum.adParamInput, 4000)
-		cmdResetPassword.Parameters.Append(prmNewPwd)
-		prmNewPwd.Value = psNewPassword
-
-		Dim prmMessage As Parameter = cmdResetPassword.CreateParameter("message", ADODB.DataTypeEnum.adVarChar, ADODB.ParameterDirectionEnum.adParamOutput, 4000)
-		cmdResetPassword.Parameters.Append(prmMessage)
-
-		Err.Clear()
-
-		cmdResetPassword.ActiveConnection = mADOCon
-
-		cmdResetPassword.Execute()
-
-		Return cmdResetPassword.Parameters("message").Value.ToString
-	End Function
-
-	Private Function ConnectionString() As String
-
-		Dim objSettings As New clsSettings
-
-		ConnectionString = objSettings.GetSQLProviderString & "Data Source=" & ServerName & ";Initial Catalog=" & Database & ";Application Name=OpenHR Self-service Intranet;DataTypeCompatibility=80;" & ";User ID=" & fixedUsername & ";Password=" & fixedPassword & ";Persist Security Info=True;"
+		End Try
 
 	End Function
 
@@ -199,21 +142,19 @@ GetUsernameFromQueryString_ERROR:
 
 	Public Property Database() As String
 		Get
-			Database = gsDatabase
+			Database = _objLogin.Database
 		End Get
 		Set(ByVal Value As String)
-			' Database passed in from the asp page
-			gsDatabase = Value
+			_objLogin.Database = Value
 		End Set
 	End Property
 
 	Public Property ServerName() As String
 		Get
-			ServerName = gsServerName
+			ServerName = _objLogin.Server
 		End Get
 		Set(ByVal Value As String)
-			' ServerName passed in from the asp page
-			gsServerName = Value
+			_objLogin.Server = Value
 		End Set
 	End Property
 
@@ -230,4 +171,5 @@ GetUsernameFromQueryString_ERROR:
 
 		End Get
 	End Property
+
 End Class
