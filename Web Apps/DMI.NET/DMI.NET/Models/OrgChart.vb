efbf3ca0d616dc1364ead1d072b8d6eb68aa7225
Lofty Mettyer <COA\lofty.mettyer>
2014-01-15 09:54:10 +0000

    Converetd org charts to use ado.net. Also fixed small bugette with pictures not displaying while I was there.
    Tried putting option strict on, but got problems with the conversion to byte(). I'm sure its a simple fix, but I can't think what it is right now.

--- Web Apps/DMI.NET/DMI.NET/Models/OrgChart.vb
+++ Web Apps/DMI.NET/DMI.NET/Models/OrgChart.vb
@@ -1,6 +1,8 @@
-﻿Imports System.IO
+﻿Option Explicit On
+Option Strict Off
+
+Imports System.IO
 Imports System.Data.SqlClient
-Imports ADODB
 Imports HR.Intranet.Server
 
 Namespace Models
@@ -19,16 +21,13 @@ Namespace Models
 
 		Public Function LoadModel() As List(Of OrgChart)
 
-			Const adStateOpen = 1
-
-
-			Dim iTopLevelRecID As Integer = CleanNumeric(HttpContext.Current.Session("TopLevelRecID"))
+			Dim iTopLevelRecID As Integer = CInt(CleanNumeric(HttpContext.Current.Session("TopLevelRecID")))
 
 			Dim objSession As SessionInfo = CType(HttpContext.Current.Session("SessionContext"), SessionInfo)
 			Dim objDataAccess As New clsDataAccess(objSession.LoginInfo)
 
 			If iTopLevelRecID = 0 Then
-				Dim iSingleRecordViewID As Integer = CleanNumeric(HttpContext.Current.Session("SingleRecordViewID"))
+				Dim iSingleRecordViewID As Integer = CInt(CleanNumeric(HttpContext.Current.Session("SingleRecordViewID")))
 
 				Dim prmRecordID = New SqlParameter("piRecordID", SqlDbType.Int)
 				prmRecordID.Direction = ParameterDirection.Output
@@ -40,110 +39,95 @@ Namespace Models
 
 				objDataAccess.GetDataSet("spASRIntGetSelfServiceRecordID", prmRecordID, prmRecordCount, New SqlParameter("piViewID", iSingleRecordViewID))
 
-				If Err.Number = 0 And prmRecordCount.Value = 1 Then
+				If Err.Number = 0 And CInt(prmRecordCount.Value) = 1 Then
 					' Only one record.
-					iTopLevelRecID = CLng(prmRecordID.Value)
+					iTopLevelRecID = CInt(prmRecordID.Value)
 				End If
 			End If
 
-
-			Dim cmdThousandFindColumns = CreateObject("ADODB.Command")
-			cmdThousandFindColumns.CommandText = "spASRIntOrgChart"
-			cmdThousandFindColumns.CommandType = 4 ' Stored Procedure
-			cmdThousandFindColumns.ActiveConnection = HttpContext.Current.Session("databaseConnection")
-			cmdThousandFindColumns.CommandTimeout = 180
-
-			Dim prmRootID = cmdThousandFindColumns.CreateParameter("RootID", 3, 1)
-			cmdThousandFindColumns.Parameters.Append(prmRootID)
-			prmRootID.value = iTopLevelRecID
-
-			Err.Clear()
-			HttpContext.Current.Session("ErrorText") = ""
-
-			Dim rstHierarchyRecords As ADODB.Recordset
+			Dim orgCharts = New List(Of OrgChart)
 			Dim sErrorDescription = ""
 
 			Try
-				rstHierarchyRecords = cmdThousandFindColumns.Execute
+				Dim rstHierarchyRecords = objDataAccess.GetDataTable("spASRIntOrgChart", CommandType.StoredProcedure _
+							, New SqlParameter("RootID", SqlDbType.Int) With {.Value = iTopLevelRecID})
+
+
+
+				Dim additionalClasses As String
+
+				If rstHierarchyRecords.Rows.Count = 0 Then
+					' No records returned
+					sErrorDescription = "Error generating Organisation Chart. No matching records found."
+				Else
+
+					For Each objRow As DataRow In rstHierarchyRecords.Rows
+
+						additionalClasses = " ui-corner-all"
+
+						' highlight the current user's node
+						If CInt(objRow(0)) = iTopLevelRecID Then
+							additionalClasses &= " ui-state-active"
+						Else
+							additionalClasses &= " ui-state-default"
+						End If
+
+						' resize Photos to 48x48px
+						Dim photoSource As String = ""
+
+						If Not IsDBNull(objRow(7)) And rstHierarchyRecords.Columns(7).DataType.Name.ToLower = "byte[]" Then
+
+							Dim oleType As Short = Val(Encoding.UTF8.GetString(objRow(7), 8, 2))
+							If oleType = 2 Then	'Embeded
+								Dim abtImage = CType(objRow(7), Byte())
+								Dim binaryData As Byte() = New Byte(abtImage.Length - 400) {}
+								Try
+									Buffer.BlockCopy(abtImage, 400, binaryData, 0, abtImage.Length - 400)
+									'Create an image based on the embeded (Base64) image and resize it to 48x48
+
+									Dim ms As New MemoryStream(binaryData)
+									Dim img As Drawing.Image = Drawing.Image.FromStream(ms, True)
+
+									img = img.GetThumbnailImage(48, 48, Nothing, IntPtr.Zero)
+									photoSource = "data:image/jpeg;base64," & ImageToBase64String(img)
+								Catch exp As ArgumentNullException
+									photoSource = "../Content/images/anonymous.png"
+								End Try
+							ElseIf oleType = 3 Then	'Link
+								Dim unc As String = Trim(Encoding.UTF8.GetString(objRow(7), 290, 60))
+								Dim fileName As String = Trim(Path.GetFileName(Encoding.UTF8.GetString(objRow(7), 10, 70))).Replace("\", "/")
+								Dim fullPath As String = Trim(Encoding.UTF8.GetString(objRow(7), 80, 210)).Replace("\", "/")
+								photoSource = "file:///" & unc & "/" & fullPath & "/" & fileName
+							End If
+						Else 'No picture is defined for user, use anonymous one
+							photoSource = "../Content/images/anonymous.png"
+						End If
+
+
+						orgCharts.Add(New OrgChart() With {
+								.EmployeeID = CInt(objRow(0)),
+								.EmployeeForenames = objRow(1).ToString(),
+								.EmployeeSurname = objRow(2).ToString(),
+								.EmployeeStaffNo = objRow(3).ToString(),
+								.LineManagerStaffNo = objRow(4).ToString(),
+								.EmployeeJobTitle = objRow(5).ToString(),
+								.HierarchyLevel = CInt(objRow(6)),
+								.PhotoPath = photoSource,
+								.AbsenceTypeClass = objRow(8).ToString() & additionalClasses & " " &
+																 objRow(9).ToString() & " " &
+																 objRow(10).ToString() & " "})
+
+					Next
+				End If
+
+
+
 			Catch ex As Exception
-				rstHierarchyRecords = Nothing
 				sErrorDescription = "Error generating Organisation Chart." & vbCrLf & ex.Message
+				HttpContext.Current.Session("ErrorText") = sErrorDescription
+
 			End Try
 
-			Dim orgCharts = New List(Of OrgChart)
-
-			If Len(sErrorDescription) = 0 Then
-				If rstHierarchyRecords.State = adStateOpen Then
-					Dim additionalClasses As String
-
-					If rstHierarchyRecords.EOF And rstHierarchyRecords.BOF Then
-						' No records returned
-						sErrorDescription = "Error generating Organisation Chart. No matching records found."
-					Else
-						Do While Not rstHierarchyRecords.EOF
-							additionalClasses = " ui-corner-all"
-
-							' highlight the current user's node
-							If CType(rstHierarchyRecords.Fields(0).Value, String) = CType(iTopLevelRecID, String) Then
-								additionalClasses &= " ui-state-active"
-							Else
-								additionalClasses &= " ui-state-default"
-							End If
-
-							' resize Photos to 48x48px
-							Dim photoSource As String = ""
-
-							If Not IsDBNull(rstHierarchyRecords.Fields(7).Value) And rstHierarchyRecords.Fields(7).Type = DataTypeEnum.adVarBinary Then
-								Dim oleType As Short = Val(Encoding.UTF8.GetString(rstHierarchyRecords.Fields(7).Value, 8, 2))
-								If oleType = 2 Then	'Embeded
-									Dim abtImage = CType(rstHierarchyRecords.Fields(7).Value, Byte())
-									Dim binaryData As Byte() = New Byte(abtImage.Length - 400) {}
-									Try
-										Buffer.BlockCopy(abtImage, 400, binaryData, 0, abtImage.Length - 400)
-										'Create an image based on the embeded (Base64) image and resize it to 48x48
-
-										Dim ms As New MemoryStream(binaryData)
-										Dim img As Drawing.Image = Drawing.Image.FromStream(ms, True)
-
-										img = img.GetThumbnailImage(48, 48, Nothing, IntPtr.Zero)
-										photoSource = "data:image/jpeg;base64," & ImageToBase64String(img)
-									Catch exp As ArgumentNullException
-										photoSource = "../Content/images/anonymous.png"
-									End Try
-								ElseIf oleType = 3 Then	'Link
-									Dim unc As String = Trim(Encoding.UTF8.GetString(rstHierarchyRecords.Fields(7).Value, 290, 60))
-									Dim fileName As String = Trim(Path.GetFileName(Encoding.UTF8.GetString(rstHierarchyRecords.Fields(7).Value, 10, 70))).Replace("\", "/")
-									Dim fullPath As String = Trim(Encoding.UTF8.GetString(rstHierarchyRecords.Fields(7).Value, 80, 210)).Replace("\", "/")
-									photoSource = "file:///" & unc & "/" & fullPath & "/" & fileName
-								End If
-							Else 'No picture is defined for user, use anonymous one
-								photoSource = "../Content/images/anonymous.png"
-							End If
-
-
-							orgCharts.Add(New OrgChart() With {
-									.EmployeeID = rstHierarchyRecords.Fields(0).Value,
-									.EmployeeForenames = rstHierarchyRecords.Fields(1).Value,
-									.EmployeeSurname = rstHierarchyRecords.Fields(2).Value,
-									.EmployeeStaffNo = rstHierarchyRecords.Fields(3).Value,
-									.LineManagerStaffNo = rstHierarchyRecords.Fields(4).Value,
-									.EmployeeJobTitle = rstHierarchyRecords.Fields(5).Value,
-									.HierarchyLevel = rstHierarchyRecords.Fields(6).Value,
-								.PhotoPath = photoSource,
-									.AbsenceTypeClass = rstHierarchyRecords.Fields(8).Value & additionalClasses & " " &
-																	 rstHierarchyRecords.Fields(9).Value & " " &
-																	 rstHierarchyRecords.Fields(10).Value & " "})
-
-							rstHierarchyRecords.MoveNext()
-						Loop
-					End If
-
-				End If
-			End If
-
-			If sErrorDescription.Length > 0 Then
-				HttpContext.Current.Session("ErrorText") = sErrorDescription
-			End If
 
 			Return orgCharts
 
