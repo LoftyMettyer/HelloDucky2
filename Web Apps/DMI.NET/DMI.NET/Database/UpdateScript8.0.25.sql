9d25fcbabb1d4f945471a13583a76600eab90e1f
Roberto Caballero <COA\roberto.caballero>
2014-02-06 15:22:17 +0000

    Added UpdateScript8.0.25.sql

---
+++ Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.0.25.sql
@@ -0,0 +1,3 @@
+﻿EXEC spsys_setsystemsetting 'database', 'version', '8.0';
+EXEC spsys_setsystemsetting 'intranet', 'version', '8.0.25';
+EXEC spsys_setsystemsetting 'ssintranet', 'version', '8.0.25';
