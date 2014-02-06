be850297f02e90eee61cb266658662bc18386a56
Roberto Caballero <COA\roberto.caballero>
2014-02-06 12:23:01 +0000

    Task 5000:Deployment Task for Build 25

--- Web Apps/DMI.NET/DMI.NET/My Project/AssemblyInfo.vb
+++ Web Apps/DMI.NET/DMI.NET/My Project/AssemblyInfo.vb
@@ -9,7 +9,7 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 <Assembly: AssemblyTitle("DMI.NET")> 
 <Assembly: AssemblyDescription("OpenHR Data Manager Intranet")> 
-<Assembly: AssemblyCompany("Advanced Computer Software Ltd")> 
+<Assembly: AssemblyCompany("Advanced Computer Software Ltd.")> 
 <Assembly: AssemblyProduct("DMI.NET")> 
 <Assembly: AssemblyCopyright("Copyright © 2014")> 
 <Assembly: AssemblyTrademark("")> 
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("8.0.24")> 
-<Assembly: AssemblyFileVersion("8.0.24")> 
+<Assembly: AssemblyVersion("8.0.25")> 
+<Assembly: AssemblyFileVersion("8.0.25")> 
