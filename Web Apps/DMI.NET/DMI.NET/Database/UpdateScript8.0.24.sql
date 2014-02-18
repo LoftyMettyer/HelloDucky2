b6cf68b51488066421063fa4b4f804414ebf11f9
Lofty Mettyer <COA\lofty.mettyer>
2014-02-18 11:02:46 +0000

    Merged the misc update scripts into 1. There is a lot of duplication of functions here, which will have to stay for the moment. There are also warnings about dependencies being in the wrong order, which look messy, but shouldn't cause any issues.
    From now on we should tack any script changes to the bottom of this procedure. (If you're altering an already scripted procedure then you should take the opportunity to remove the duplicates and tidy up)
    
    Zapped the old update scripts.
    
    This will upgrade the web component of the database - you will also need to run v8 of te system manager before you can use.

--- Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.0.24.sql
+++
@@ -1,3 +0,0 @@
-﻿EXEC spsys_setsystemsetting 'database', 'version', '8.0';
-EXEC spsys_setsystemsetting 'intranet', 'version', '8.0.24';
-EXEC spsys_setsystemsetting 'ssintranet', 'version', '8.0.24';
--- Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.0.25.sql
+++
@@ -1,3 +0,0 @@
-﻿EXEC spsys_setsystemsetting 'database', 'version', '8.0';
-EXEC spsys_setsystemsetting 'intranet', 'version', '8.0.25';
-EXEC spsys_setsystemsetting 'ssintranet', 'version', '8.0.25';
