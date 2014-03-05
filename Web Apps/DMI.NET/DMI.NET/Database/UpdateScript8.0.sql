4b98036044eb4f9a49e6f1d97d9904c00c8e170f
Lofty Mettyer <COA\lofty.mettyer>
2014-03-05 11:54:16 +0000

    Another whoops. I'm having an off day today. Missed out adding this rather important sp into the update script.

--- Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.0.sql
+++ Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.0.sql
@@ -2,6 +2,10 @@
 DROP PROCEDURE [dbo].[sp_ASR_AbsenceBreakdown_Run]
 DROP PROCEDURE [dbo].[sp_ASR_Bradford_DeleteAbsences]
 
+IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spASRIntGetLoginDetails]') AND xtype in (N'P'))
+	DROP PROCEDURE [dbo].[spASRIntGetLoginDetails]
+GO
+
 IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[sp_ASRIntCheckLogin]') AND xtype in (N'P'))
 	DROP PROCEDURE [dbo].[sp_ASRIntCheckLogin]
 GO
@@ -86123,6 +86127,61 @@ BEGIN
 		WHEN @iCount > 0 THEN 1
 		ELSE 0
 	END;
+END
+
+GO
+CREATE PROCEDURE dbo.[spASRIntGetLoginDetails]
+AS
+BEGIN
+
+	DECLARE @psUserName			nvarchar(MAX), 
+			@psUserGroup		nvarchar(MAX),
+			@piUserGroupID		integer,
+			@licenseKey			varchar(MAX),
+			@sSysManagerVersion	varchar(200),
+			@sIntranetDBVersion	varchar(200),
+			@bIsLocked			bit = 0,
+			@sLockMessage		varchar(MAX) = '',
+			@bUpdateInProgress	bit = 0;
+
+	-- Get security info for this user
+	EXEC [dbo].[spASRIntGetActualUserDetails] @psUserName OUTPUT, @psUserGroup OUTPUT, @piUserGroupID OUTPUT
+	SELECT @psUserName, @psUserGroup, @piUserGroupID
+
+	-- DB information
+	SELECT @licenseKey = SettingValue FROM ASRSysSystemSettings WHERE section = 'Licence' AND SettingKey = 'Key';		
+	SELECT @sSysManagerVersion = SettingValue FROM ASRSysSystemSettings WHERE section = 'database' AND SettingKey = 'version';		
+	SELECT @sIntranetDBVersion = SettingValue FROM ASRSysSystemSettings WHERE section = 'intranet' AND SettingKey = 'version';		
+
+	-- Lock information
+	IF EXISTS(SELECT * FROM ASRSysLock WHERE Priority = 1)
+	BEGIN
+		SELECT @sLockMessage = SettingValue FROM ASRSysSystemSettings WHERE section = 'messaging' AND SettingKey = 'lockmessage';
+		SET @bUpdateInProgress = 1;
+	END
+
+	IF EXISTS(SELECT * FROM ASRSysLock WHERE Priority = 2)
+	BEGIN
+		SET @bIsLocked = 1;
+		SET @bUpdateInProgress = 1;
+	END
+
+	SELECT @licenseKey AS LicenseKey, @sSysManagerVersion AS SysMgrDBVersion, @sIntranetDBVersion AS IntDBVersion,
+			@bUpdateInProgress AS UpdateInProgress, @bIsLocked AS IsLocked, @sLockMessage AS LockMessage
+
+	-- Permissions for this user
+	SELECT c.categoryKey, i.itemKey, i.categoryID, g.itemID, g.permitted AS permitted
+		FROM [ASRSysGroupPermissions] g
+			INNER JOIN ASRSysPermissionItems i ON i.itemID = g.itemID
+			INNER JOIN ASRSysPermissionCategories c ON c.categoryID = i.categoryID
+		WHERE g.groupName = @psUserGroup
+		ORDER BY i.categoryID;
+
+	-- Server roles
+	SELECT IS_SRVROLEMEMBER('serveradmin') AS IsServerAdmin
+		, IS_SRVROLEMEMBER('securityadmin') AS IsSecurityAdmin
+		, IS_SRVROLEMEMBER('sysadmin') AS IsSysAdmin;
+
 END
 
 
