975894c09bab179e31c1b73ae4593d245cb805ee
Lofty Mettyer <COA\lofty.mettyer>
2014-03-17 13:47:27 +0000

    JIRA-3790 (groundwork). We need to move the scope declarations over to classes to avoid spillover of data between sessions. This is gorundwork for this task.
    There are no functional changes in this chageset (hopefully!)

---
+++ Components/COAInt_Server/ModuleSpecifics/Consts.vb
@@ -0,0 +1,80 @@
+﻿Namespace ModuleSpecifics
+
+	Module Consts
+
+#Region "From modAbsenceSpecifics"
+		Friend Const gsMODULEKEY_ABSENCE As String = "MODULE_ABSENCE"
+		Friend Const gsPARAMETERKEY_ABSENCETABLE As String = "Param_TableAbsence"
+		Friend Const gsPARAMETERKEY_ABSENCETYPETABLE As String = "Param_TableAbsenceType"
+		Friend Const gsPARAMETERKEY_ABSENCESTARTDATE As String = "Param_FieldStartDate"
+		Friend Const gsPARAMETERKEY_ABSENCESTARTSESSION As String = "Param_FieldStartSession"
+		Friend Const gsPARAMETERKEY_ABSENCEENDDATE As String = "Param_FieldEndDate"
+		Friend Const gsPARAMETERKEY_ABSENCEENDSESSION As String = "Param_FieldEndSession"
+		Friend Const gsPARAMETERKEY_ABSENCETYPE As String = "Param_FieldType"
+		Friend Const gsPARAMETERKEY_ABSENCEREASON As String = "Param_FieldReason"
+		Friend Const gsPARAMETERKEY_ABSENCEDURATION As String = "Param_FieldDuration"
+		Friend Const gsPARAMETERKEY_ABSENCECONTINUOUS As String = "Param_FieldContinuous"
+		Friend Const gsPARAMETERKEY_ABSENCETYPETYPE As String = "Param_FieldTypeType"
+		Friend Const gsPARAMETERKEY_ABSENCETYPECODE As String = "Param_FieldTypeCode"
+		Friend Const gsPARAMETERKEY_ABSENCETYPESSP As String = "Param_FieldTypeSSP"
+		Friend Const gsPARAMETERKEY_ABSENCETYPECALCODE As String = "Param_FieldTypeCalCode"
+		Friend Const gsPARAMETERKEY_ABSENCETYPEINCLUDE As String = "Param_FieldTypeInclude"
+		Friend Const gsPARAMETERKEY_ABSENCETYPEBRADFORDINDEX As String = "Param_FieldTypeBradfordIndex"
+		Friend Const gsPARAMETERKEY_ABSENCECALSTARTMONTH As String = "Param_FieldStartMonth"
+		Friend Const gsPARAMETERKEY_ABSENCECALWEEKENDSHADING As String = "Param_OtherWeekendShading"
+		Friend Const gsPARAMETERKEY_ABSENCECALBHOLSHADING As String = "Param_OtherBHolShading"
+		Friend Const gsPARAMETERKEY_ABSENCECALINCLUDEWORKINGDAYSONLY As String = "Param_OtherIncludeWorkingsDaysOnly"
+		Friend Const gsPARAMETERKEY_ABSENCECALBHOLINCLUDE As String = "Param_OtherBHolInclude"
+		Friend Const gsPARAMETERKEY_ABSENCECALSHOWCAPTIONS As String = "Param_OtherShowCaptions"
+#End Region
+
+#Region "From modBankHolidaySpecifics"
+		Friend Const gsPARAMETERKEY_BHOLREGIONTABLE As String = "Param_TableBHolRegion"
+		Friend Const gsPARAMETERKEY_BHOLREGION As String = "Param_FieldBHolRegion"
+		Friend Const gsPARAMETERKEY_BHOLTABLE As String = "Param_TableBHol"
+		Friend Const gsPARAMETERKEY_BHOLDATE As String = "Param_FieldBHolDate"
+		Friend Const gsPARAMETERKEY_BHOLDESCRIPTION As String = "Param_FieldBHolDescription"
+#End Region
+
+#Region "From modPersonnelSpecifics"
+		Friend Const gsMODULEKEY_PERSONNEL As String = "MODULE_PERSONNEL"
+		Friend Const gsPARAMETERKEY_PERSONNELTABLE As String = "Param_TablePersonnel"
+		Friend Const gsPARAMETERKEY_EMPLOYEENUMBER As String = "Param_FieldsEmployeeNumber"
+		Friend Const gsPARAMETERKEY_FORENAME As String = "Param_FieldsForename"
+		Friend Const gsPARAMETERKEY_SURNAME As String = "Param_FieldsSurname"
+		Friend Const gsPARAMETERKEY_STARTDATE As String = "Param_FieldsStartDate"
+		Friend Const gsPARAMETERKEY_LEAVINGDATE As String = "Param_FieldsLeavingDate"
+		Friend Const gsPARAMETERKEY_FULLPARTTIME As String = "Param_FieldsFullPartTime"
+		Friend Const gsPARAMETERKEY_EMAIL As String = "Param_FieldsEmail"
+		Friend Const gsPARAMETERKEY_DEPARTMENT As String = "Param_FieldsDepartment"
+
+		'Region Constants - The following key is used for static region field
+		Friend Const gsPARAMETERKEY_REGION As String = "Param_FieldsRegion"
+		'Region Constants - The following keys are used for historical region fields
+		Friend Const gsPARAMETERKEY_HREGIONTABLE As String = "Param_FieldsHRegionTable"
+		Friend Const gsPARAMETERKEY_HREGIONFIELD As String = "Param_FieldsHRegion"
+		Friend Const gsPARAMETERKEY_HREGIONDATE As String = "Param_FieldsHRegionDate"
+
+		'WP Constants - The following key is used for static WP field
+		Friend Const gsPARAMETERKEY_WORKINGPATTERN As String = "Param_FieldsWorkingPattern"
+		'WP Constants - The following keys are used for historical WP fields
+		Friend Const gsPARAMETERKEY_HWORKINGPATTERNTABLE As String = "Param_FieldsHWorkingPatternTable"
+		Friend Const gsPARAMETERKEY_HWORKINGPATTERNFIELD As String = "Param_FieldsHWorkingPattern"
+		Friend Const gsPARAMETERKEY_HWORKINGPATTERNDATE As String = "Param_FieldsHWorkingPatternDate"
+
+		' HIERARCHY MODULE CONSTANTS
+		Friend Const gsMODULEKEY_HIERARCHY As String = "MODULE_HIERARCHY"
+		Friend Const gsPARAMETERKEY_HIERARCHYTABLE As String = "Param_TableHierarchy"
+		Friend Const gsPARAMETERKEY_IDENTIFIER As String = "Param_FieldIdentifier"
+#End Region
+
+#Region "From modTrainingBookingSpecifics"
+		Friend Const gsMODULEKEY_TRAININGBOOKING As String = "MODULE_TRAININGBOOKING"
+		Friend Const gsPARAMETERKEY_TRAINBOOKTABLE As String = "Param_TrainBookTable"
+		Friend Const gsPARAMETERKEY_COURSESTARTDATE As String = "Param_CourseStartDate"
+		Friend Const gsPARAMETERKEY_COURSEENDDATE As String = "Param_CourseEndDate"
+#End Region
+
+	End Module
+
+End Namespace
