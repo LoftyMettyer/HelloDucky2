9f47c7d9e5102d90fa3751053143d4f577b3efe5
Nick Gibson <COA\nick.gibson>
2014-03-18 13:15:28 +0000

    Bug 6803: Custom Reports to excel - Page Break setting is being ignored

--- Components/COAInt_Server/CustomReportsRUN.vb
+++ Components/COAInt_Server/CustomReportsRUN.vb
@@ -2617,9 +2617,9 @@ CheckRecordSet_ERROR:
 							Else
 								If objReportItem.IsBitColumn Then	'Bit
 									'UPGRADE_WARNING: Couldn't resolve default property of object vValue. Click for more: 'ms-help://MS.VSCC.v90/dv_commoner/local/redirect.htm?keyword="6A50421D-15FE-4896-8A1B-2EC21E9037B2"'
-									fBreak = (RTrim(LCase(vValue)) <> RTrim(LCase(objReportItem.LastValue)))
-								Else
 									fBreak = Not (vValue = objReportItem.LastValue)
+								Else
+									fBreak = (RTrim(LCase(vValue)) <> RTrim(LCase(objReportItem.LastValue)))
 								End If
 							End If
 
