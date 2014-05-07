e5e00edfaf894a4cd0428bd98907c4e15de9ce97
Roberto Caballero <COA\roberto.caballero>
2014-05-07 10:38:51 +0100

    Task 8464:Deployment Task for Build 34

--- Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.0.sql
+++ Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.0.sql
@@ -83667,7 +83667,7 @@ GO
 GRANT EXEC ON TYPE::[dbo].[DataPermissions] TO ASRSysGroup
 
 
-DECLARE @sVersion varchar(10) = '8.0.33'
+DECLARE @sVersion varchar(10) = '8.0.34'
 
 EXEC spsys_setsystemsetting 'database', 'version', '8.0';
 EXEC spsys_setsystemsetting 'intranet', 'version', @sVersion;
--- Web Apps/DMI.NET/DMI.NET/My Project/AssemblyInfo.vb
+++ Web Apps/DMI.NET/DMI.NET/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("8.0.33")> 
-<Assembly: AssemblyFileVersion("8.0.33")> 
+<Assembly: AssemblyVersion("8.0.34")> 
+<Assembly: AssemblyFileVersion("8.0.34")> 
