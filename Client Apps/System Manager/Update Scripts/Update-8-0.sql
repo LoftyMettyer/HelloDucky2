e4a33683002811cc3b98bb02ec25bbcccc23f0d0
Lofty Mettyer <COA\lofty.mettyer>
2014-05-08 15:13:11 +0100

    JIRA-4140 - Arithmetic overfl;ow if you put crap data in the create exact date function

--- Client Apps/System Manager/Update Scripts/Update-8-0.sql
+++ Client Apps/System Manager/Update Scripts/Update-8-0.sql
@@ -209,7 +209,7 @@ PRINT 'Step - New functions'
 
 			DECLARE @date varchar(20);
 
-			IF @day = 0 OR @month = 0 OR @year = 0 RETURN NULL;
+			IF @day < 1 OR @month < 1 OR @year < 1 OR @month > 12 OR @day > 31 OR @year > 9999  RETURN NULL;
 			SET @date = CONVERT(varchar(2), @month) + ''/'' + CONVERT(varchar(2), @day) + ''/'' + CONVERT(varchar(4), @year);
 
 			IF ISDATE(@date) = 0
