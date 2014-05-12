45d49d5356dab9e48041e917acc9028633a87f0f
Nick Gibson <COA\nick.gibson>
2014-05-12 13:02:45 +0100

    Initial Check-in

---
+++ Prototypes/ABS Self Service Portal/ABS Self Service/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ABS_Self_Service.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace ABS_Self_Service.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var currentUserId = User.Identity.GetUserId();
+            if (currentUserId != null) {
+                var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                var currentUser = manager.FindById(currentUserId);
+                ViewBag.OpenHRID = currentUser.OpenHRID;
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Prototypes/ABS Self Service Portal/ABS Self Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ABS_Self_Service.MvcApplication" Language="C#" %>
