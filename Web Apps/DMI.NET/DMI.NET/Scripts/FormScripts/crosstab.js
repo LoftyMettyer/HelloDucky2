b13afd86c5c499b90c91c042f1c87821ea7c9824
Nick Gibson <COA\nick.gibson>
2014-10-15 15:15:33 +0100

    Task 10624: Cross Tabs - Report Preview screen - Type and Value dropdowns are empty (Firefox Only)

--- Web Apps/DMI.NET/DMI.NET/Scripts/FormScripts/crosstab.js
+++ Web Apps/DMI.NET/DMI.NET/Scripts/FormScripts/crosstab.js
@@ -1,95 +1,41 @@
 ﻿
 function AddToIntTypeCombo(strText, strValue) {
-    var oOption = document.createElement("OPTION");
-    var cboIntersectionType = document.getElementById("cboIntersectionType");
-    cboIntersectionType.options.add(oOption);
-    oOption.innerText = strText;
-    oOption.Value = strValue;
+	$("#cboIntersectionType").append('<option value=' + strValue + '>' + strText + '</option>');
 }
 
 function AddToPgbCombo(strText, strValue) {
-    var oOption = document.createElement("OPTION");
-    var cboPage = document.getElementById("cboPage");
-    cboPage.options.add(oOption);
-    oOption.innerText = strText;
-    oOption.Value = strValue;
+	$("#cboPage").append('<option value=' + strValue + '>' + strText + '</option>');   
 }
 
-function refreshCombo(psComboKey) {
-    try {
-        var iSelectedIndex;
-        var objCombo;
 
-        if (psComboKey == "PAGE") {
-            objCombo = document.getElementById("cboPage");
-        }
-        else {
-            if (psComboKey == "INTERSECTIONTYPE") {
-                objCombo = document.getElementById("cboIntersectionType");
-            }
-            else {
-                objCombo = document.getElementById("cboFileFormat");
-            }
-        }
-
-        iSelectedIndex = objCombo.selectedIndex;
-
-        while (cboDummy.options.length > 0) {
-            cboDummy.options.remove(0);
-        }
-
-        while (objCombo.options.length > 0) {
-            var oOption = document.createElement("OPTION");
-            cboDummy.options.add(oOption);
-            oOption.innerText = objCombo.item(0).innerText;
-
-            // Needs both the value and the Value properties - Capital V for dropdowns to work, lowercase for drilldown to work.
-            oOption.Value = objCombo.item(0).Value;
-            oOption.value = objCombo.item(0).value;
-            objCombo.options.remove(0);
-        }
-
-        while (cboDummy.options.length > 0) {
-            var oOption = document.createElement("OPTION");
-            objCombo.options.add(oOption);
-            oOption.innerText = cboDummy.item(0).innerText;
-            oOption.Value = cboDummy.item(0).Value;
-            oOption.value = cboDummy.item(0).value;
-            cboDummy.options.remove(0);
-        }
-
-        objCombo.selectedIndex = iSelectedIndex;
-    }
-    catch (e) { }
-}
 
 function chkPercentType_Click() {
 
-    checkbox_disable(chkPercentPage, (chkPercentType.checked == false));
-    if (chkPercentType.checked == false) {
-        chkPercentPage.checked = false;
-    }
-    UpdateGrid();
+		checkbox_disable(chkPercentPage, (chkPercentType.checked == false));
+		if (chkPercentType.checked == false) {
+				chkPercentPage.checked = false;
+		}
+		UpdateGrid();
 }
 
 function UpdateGrid() {
 
-    var strMode = "REFRESH";
+		var strMode = "REFRESH";
 
-    var lngPageNumber = 0;
-    if (window.cboPage.selectedIndex != -1) {
-        lngPageNumber = window.cboPage.options[window.cboPage.selectedIndex].Value;
-    }
+		var lngPageNumber = 0;
+		if (window.cboPage.selectedIndex != -1) {
+				lngPageNumber = window.cboPage.options[window.cboPage.selectedIndex].value;
+		}
 
-    var lngIntType = 0;
-    if (window.cboIntersectionType.selectedIndex != -1) {
-        lngIntType = window.cboIntersectionType.options[window.cboIntersectionType.selectedIndex].Value;
-    }
+		var lngIntType = 0;
+		if (window.cboIntersectionType.selectedIndex != -1) {
+				lngIntType = window.cboIntersectionType.options[window.cboIntersectionType.selectedIndex].value;
+		}
 
-    var blnShowPer = (window.chkPercentType.checked == true);
-    var blnPerPage = (window.chkPercentPage.checked == true);
-    var blnSupZeros = (window.chkSuppressZeros.checked == true);
-    var blnThousand = (window.chkUse1000.checked == true);
+		var blnShowPer = (window.chkPercentType.checked == true);
+		var blnPerPage = (window.chkPercentPage.checked == true);
+		var blnSupZeros = (window.chkSuppressZeros.checked == true);
+		var blnThousand = (window.chkUse1000.checked == true);
 
-    getCrossTabData(strMode, lngPageNumber, lngIntType, blnShowPer, blnPerPage, blnSupZeros, blnThousand);
+		getCrossTabData(strMode, lngPageNumber, lngIntType, blnShowPer, blnPerPage, blnSupZeros, blnThousand);
 }
