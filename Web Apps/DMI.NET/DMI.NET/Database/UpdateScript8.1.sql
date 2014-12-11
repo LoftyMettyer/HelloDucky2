186264acbc3fca909c44039fa2a76e611431709f
Roberto Caballero <COA\roberto.caballero>
2014-12-11 10:20:59 +0000

    Deployment Task for Build 8.1.9

--- Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.1.sql
+++ Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.1.sql
@@ -11963,7 +11963,7 @@ DEALLOCATE curObjects
 GO
 
 		
-DECLARE @sVersion varchar(10) = '8.1.8'
+DECLARE @sVersion varchar(10) = '8.1.9'
 
 EXEC spsys_setsystemsetting 'database', 'version', '8.1';
 EXEC spsys_setsystemsetting 'intranet', 'version', @sVersion;
