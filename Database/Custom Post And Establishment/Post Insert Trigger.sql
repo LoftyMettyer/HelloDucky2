c3fabf93b7f27e84dc63e061ad4e55e2af6ef881
Mark Edwynn <COA\Mark.Edwynn>
2015-01-05 09:13:16 +0000

    Pay Scales Groups renamed to Job Families

--- Database/Custom Post And Establishment/Post Insert Trigger.sql
+++ Database/Custom Post And Establishment/Post Insert Trigger.sql
@@ -9,28 +9,28 @@ BEGIN
     SET NOCOUNT ON;
 
 	INSERT Post_Holiday_Schemes (ID_219, Effective_Date, Holiday_Scheme)
-		SELECT i.ID, i.Effective_Date, phs.Holiday_Scheme
+		SELECT i.ID, i.Effective_Date, jhs.Holiday_Scheme
 			FROM inserted i
-			INNER JOIN Pay_Scale_Groups pg ON pg.Pay_Scale_Group = i.Pay_Scale_Group
-			INNER JOIN PSG_Holiday_Schemes phs ON phs.ID_215 = pg.ID AND (phs.End_Date >= GETDATE() OR End_Date IS NULL);
+			INNER JOIN Job_Families jf ON jf.Job_Family = i.Job_Family
+			INNER JOIN Job_Family_Holiday_Schemes jhs ON jhs.ID_215 = jf.ID AND (jhs.End_Date >= GETDATE() OR End_Date IS NULL);
 
 	INSERT Post_OMP_Schemes (ID_219, Effective_Date, OMP_Scheme, Description)
 		SELECT i.ID, i.Effective_Date, omp.OMP_Scheme, omp.Description
 			FROM inserted i
-			INNER JOIN Pay_Scale_Groups pg ON pg.Pay_Scale_Group = i.Pay_Scale_Group
-			INNER JOIN PSG_OMP_Schemes omp ON omp.ID_215 = pg.ID AND (omp.End_Date >= GETDATE() OR End_Date IS NULL);
+			INNER JOIN Job_Families jf ON jf.Job_Family = i.Job_Family
+			INNER JOIN Job_Family_OMP_Schemes omp ON omp.ID_215 = jf.ID AND (omp.End_Date >= GETDATE() OR End_Date IS NULL);
 
 	INSERT Post_OSP_Schemes (ID_219, Effective_Date, OSP_Scheme, Description)
 		SELECT i.ID, i.Effective_Date, osp.OSP_Scheme, osp.Description
 			FROM inserted i
-			INNER JOIN Pay_Scale_Groups pg ON pg.Pay_Scale_Group = i.Pay_Scale_Group
-			INNER JOIN PSG_OSP_Schemes osp ON osp.ID_215 = pg.ID AND (osp.End_Date >= GETDATE() OR End_Date IS NULL);
+			INNER JOIN Job_Families jf ON jf.Job_Family = i.Job_Family
+			INNER JOIN Job_Family_OSP_Schemes osp ON osp.ID_215 = jf.ID AND (osp.End_Date >= GETDATE() OR End_Date IS NULL);
 
 	INSERT Post_Pension_Schemes (ID_219, Effective_Date, Pension_Scheme)
 		SELECT i.ID, i.Effective_Date, pen.Pension_Scheme
 			FROM inserted i
-			INNER JOIN Pay_Scale_Groups pg ON pg.Pay_Scale_Group = i.Pay_Scale_Group
-			INNER JOIN PSG_Pension_Schemes pen ON pen.ID_215 = pg.ID AND (pen.End_Date >= GETDATE() OR End_Date IS NULL);
+			INNER JOIN Job_Families jf ON jf.Job_Family = i.Job_Family
+			INNER JOIN Job_Family_Pension_Schemes pen ON pen.ID_215 = jf.ID AND (pen.End_Date >= GETDATE() OR End_Date IS NULL);
 
 	INSERT Post_Working_Patterns (ID_219, Effective_Date, Regional_ID, Absence_In, Day_Pattern, Sunday_Hours_AM, Sunday_Hours_PM, Monday_Hours_AM, Monday_Hours_PM
 									, Tuesday_Hours_AM, Tuesday_Hours_PM, Wednesday_Hours_AM, Wednesday_Hours_PM, Thursday_Hours_AM, Thursday_Hours_PM
@@ -39,8 +39,8 @@ BEGIN
 									, wp.Tuesday_Hours_AM, wp.Tuesday_Hours_PM, wp.Wednesday_Hours_AM, wp.Wednesday_Hours_PM, wp.Thursday_Hours_AM, wp.Thursday_Hours_PM
 									, wp.Friday_Hours_AM, wp.Friday_Hours_PM, wp.Saturday_Hours_AM, wp.Saturday_Hours_PM
 			FROM inserted i
-			INNER JOIN Pay_Scale_Groups pg ON pg.Pay_Scale_Group = i.Pay_Scale_Group
-			INNER JOIN PSG_Working_Patterns wp ON wp.ID_215 = pg.ID AND (wp.End_Date >= GETDATE() OR End_Date IS NULL);
+			INNER JOIN Job_Families jf ON jf.Job_Family = i.Job_Family
+			INNER JOIN Job_Family_Working_Patterns wp ON wp.ID_215 = jf.ID AND (wp.End_Date >= GETDATE() OR End_Date IS NULL);
 
 
 
