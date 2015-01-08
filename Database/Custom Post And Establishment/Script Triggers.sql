4a4677b3f11182d9a3a2f747dc02601c354d3bcb
Mark Edwynn <COA\Mark.Edwynn>
2015-01-08 17:09:37 +0000

    Job Families changed to Contract Templates

--- Database/Custom Post And Establishment/Script Triggers.sql
+++ Database/Custom Post And Establishment/Script Triggers.sql
@@ -259,28 +259,28 @@ INSERT ASRSysTableTriggers (TriggerID, TableID, Name, CodePosition, IsSystem, Co
 GO
 
 INSERT ASRSysTableTriggers (TriggerID, TableID, Name, CodePosition, IsSystem, Content) VALUES (7, 219, 'Transfer working pattern from appointment', 1, 1, '    INSERT Post_Holiday_Schemes (ID_219, Effective_Date, Holiday_Scheme)
-		SELECT i.ID, i.Effective_Date, jhs.Holiday_Scheme
+		SELECT i.ID, i.Effective_Date, chs.Holiday_Scheme
 			FROM inserted i
-			INNER JOIN Job_Families jf ON jf.Job_Family = i.Job_Family
-			INNER JOIN Job_Family_Holiday_Schemes jhs ON jhs.ID_215 = jf.ID AND (jhs.End_Date >= GETDATE() OR End_Date IS NULL);
+			INNER JOIN Contract_Templates ct ON ct.Contract = i.Contract
+			INNER JOIN Contract_Holiday_Schemes chs ON chs.ID_215 = ct.ID AND (chs.End_Date >= GETDATE() OR End_Date IS NULL);
 
 	INSERT Post_OMP_Schemes (ID_219, Effective_Date, OMP_Scheme, Description)
 		SELECT i.ID, i.Effective_Date, omp.OMP_Scheme, omp.Description
 			FROM inserted i
-			INNER JOIN Job_Families jf ON jf.Job_Family = i.Job_Family
-			INNER JOIN Job_Family_OMP_Schemes omp ON omp.ID_215 = jf.ID AND (omp.End_Date >= GETDATE() OR End_Date IS NULL);
+			INNER JOIN Contract_Templates ct ON ct.Contract = i.Contract
+			INNER JOIN Contract_OMP_Schemes omp ON omp.ID_215 = ct.ID AND (omp.End_Date >= GETDATE() OR End_Date IS NULL);
 
 	INSERT Post_OSP_Schemes (ID_219, Effective_Date, OSP_Scheme, Description)
 		SELECT i.ID, i.Effective_Date, osp.OSP_Scheme, osp.Description
 			FROM inserted i
-			INNER JOIN Job_Families jf ON jf.Job_Family = i.Job_Family
-			INNER JOIN Job_Family_OSP_Schemes osp ON osp.ID_215 = jf.ID AND (osp.End_Date >= GETDATE() OR End_Date IS NULL);
+			INNER JOIN Contract_Templates ct ON ct.Contract = i.Contract
+			INNER JOIN Contract_OSP_Schemes osp ON osp.ID_215 = ct.ID AND (osp.End_Date >= GETDATE() OR End_Date IS NULL);
 
 	INSERT Post_Pension_Schemes (ID_219, Effective_Date, Pension_Scheme)
 		SELECT i.ID, i.Effective_Date, pen.Pension_Scheme
 			FROM inserted i
-			INNER JOIN Job_Families jf ON jf.Job_Family = i.Job_Family
-			INNER JOIN Job_Family_Pension_Schemes pen ON pen.ID_215 = jf.ID AND (pen.End_Date >= GETDATE() OR End_Date IS NULL);
+			INNER JOIN Contract_Templates ct ON ct.Contract = i.Contract
+			INNER JOIN Contract_Pension_Schemes pen ON pen.ID_215 = ct.ID AND (pen.End_Date >= GETDATE() OR End_Date IS NULL);
 
 	INSERT Post_Working_Patterns (ID_219, Effective_Date, Regional_ID, Absence_In, Day_Pattern, Sunday_Hours_AM, Sunday_Hours_PM, Monday_Hours_AM, Monday_Hours_PM
 									, Tuesday_Hours_AM, Tuesday_Hours_PM, Wednesday_Hours_AM, Wednesday_Hours_PM, Thursday_Hours_AM, Thursday_Hours_PM
@@ -289,7 +289,7 @@ INSERT ASRSysTableTriggers (TriggerID, TableID, Name, CodePosition, IsSystem, Co
 									, wp.Tuesday_Hours_AM, wp.Tuesday_Hours_PM, wp.Wednesday_Hours_AM, wp.Wednesday_Hours_PM, wp.Thursday_Hours_AM, wp.Thursday_Hours_PM
 									, wp.Friday_Hours_AM, wp.Friday_Hours_PM, wp.Saturday_Hours_AM, wp.Saturday_Hours_PM
 			FROM inserted i
-			INNER JOIN Job_Families jf ON jf.Job_Family = i.Job_Family
-			INNER JOIN Job_Family_Working_Patterns wp ON wp.ID_215 = jf.ID AND (wp.End_Date >= GETDATE() OR End_Date IS NULL);');
+			INNER JOIN Contract_Templates ct ON ct.Contract = i.Contract
+			INNER JOIN Contract_Working_Patterns wp ON wp.ID_215 = ct.ID AND (wp.End_Date >= GETDATE() OR End_Date IS NULL);');
 
 GO
