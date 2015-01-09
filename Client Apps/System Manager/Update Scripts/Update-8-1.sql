f358fde4bc8683c2ec4a2a04b4c45aabe0fa451b
Lofty Mettyer <COA\lofty.mettyer>
2015-01-09 12:57:30 +0000

    Modified the absence entry trigger so that it now looks at the working patterns for each post and puts entries in for each post regardless of duration. This should fix the absence rolup chopping dates off the start and end of the absence if no working pattern is defined. It should also now handle if there are overlapping working patterns for a appointment.

--- Client Apps/System Manager/Update Scripts/Update-8-1.sql
+++ Client Apps/System Manager/Update Scripts/Update-8-1.sql
@@ -4356,6 +4356,47 @@ PRINT 'Step - P&E Core functions'
 		RETURN;
 	END';
 
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[udfsysPatternFromHours]') AND xtype = 'FN')
+		DROP FUNCTION [dbo].[udfsysPatternFromHours];
+
+	EXEC sp_executesql N'CREATE FUNCTION [dbo].[udfsysPatternFromHours] (
+	@PatternType	varchar(5),
+	@Sunday_Hours numeric(4,2),
+	@Monday_Hours numeric(4,2),
+	@Tuesday_Hours numeric(4,2),
+	@Wednesday_Hours numeric(4,2),
+	@Thursday_Hours numeric(4,2),
+	@Friday_Hours numeric(4,2),
+	@Saturday_Hours numeric(4,2))
+RETURNS varchar(28)
+AS 
+BEGIN
+
+	DECLARE @value varchar(28);
+
+	IF @PatternType = ''Days''
+		SET @value = CASE WHEN @Sunday_Hours > 0 THEN ''1'' ELSE ''0'' END +
+						CASE WHEN @Monday_Hours > 0 THEN ''1'' ELSE ''0'' END +
+						CASE WHEN @Tuesday_Hours > 0 THEN ''1'' ELSE ''0'' END +
+						CASE WHEN @Wednesday_Hours > 0 THEN ''1'' ELSE ''0'' END +
+						CASE WHEN @Thursday_Hours > 0 THEN ''1'' ELSE ''0'' END +
+						CASE WHEN @Friday_Hours > 0 THEN ''1'' ELSE ''0'' END +
+						CASE WHEN @Saturday_Hours > 0 THEN ''1'' ELSE ''0'' END;
+	ELSE
+		SET @value = CASE WHEN ISNULL(@Sunday_Hours,0) > 0 THEN REPLACE(RIGHT(''00000'' + CONVERT(varchar(5), @Sunday_Hours), 5), ''.'','''') ELSE ''0000'' END +
+						CASE WHEN ISNULL(@Monday_Hours,0) > 0 THEN REPLACE(RIGHT(''00000'' + CONVERT(varchar(5), @Monday_Hours), 5), ''.'','''') ELSE ''0000'' END +
+						CASE WHEN ISNULL(@Tuesday_Hours,0) > 0 THEN REPLACE(RIGHT(''00000'' + CONVERT(varchar(5), @Tuesday_Hours), 5), ''.'','''') ELSE ''0000'' END +
+						CASE WHEN ISNULL(@Wednesday_Hours,0) > 0 THEN REPLACE(RIGHT(''00000'' + CONVERT(varchar(5), @Wednesday_Hours), 5), ''.'','''') ELSE ''0000'' END +
+						CASE WHEN ISNULL(@Thursday_Hours,0) > 0 THEN REPLACE(RIGHT(''00000'' + CONVERT(varchar(5), @Thursday_Hours), 5), ''.'','''') ELSE ''0000'' END +
+						CASE WHEN ISNULL(@Friday_Hours,0) > 0 THEN REPLACE(RIGHT(''00000'' + CONVERT(varchar(5), @Friday_Hours), 5), ''.'','''') ELSE ''0000'' END +
+						CASE WHEN ISNULL(@Saturday_Hours,0) > 0 THEN REPLACE(RIGHT(''00000'' + CONVERT(varchar(5), @Saturday_Hours), 5), ''.'','''') ELSE ''0000'' END;
+
+	RETURN @value;
+
+END';
+
+
+
 
 /* ------------------------------------------------------- */
 PRINT 'Step - Table Triggers'
--- Database/Custom Post And Establishment/Simple Test Harness.sql
+++ Database/Custom Post And Establishment/Simple Test Harness.sql
@@ -15,22 +15,30 @@ DELETE FROM Appointment_Absence_Entry
 DELETE FROM Absence_Breakdown
 DELETE FROM Absence
 
-    /* This sets all of the flags prior to updating date dependant columns */
-    DELETE FROM ASRSYSSystemSettings WHERE [Section] = 'database' and [SettingKey] = 'updatingdatedependantcolumns'
+    --/* This sets all of the flags prior to updating date dependant columns */
+    --DELETE FROM ASRSYSSystemSettings WHERE [Section] = 'database' and [SettingKey] = 'updatingdatedependantcolumns'
 
-    INSERT ASRSYSSystemSettings([Section],[SettingKey],[SettingValue])
-    VALUES('database','updatingdatedependantcolumns',1)
+    --INSERT ASRSYSSystemSettings([Section],[SettingKey],[SettingValue])
+    --VALUES('database','updatingdatedependantcolumns',1)
 
 print 'iNSERT'
+--delete from Appointment_Working_Patterns
 --INSERT Appointment_Absence_Entry (ID_3, start_date, start_session, end_date, end_session, Reason, absence_type) values (825, getdate()-1000, 'AM', getdate()-995, 'PM', 'multiple post day off', 'HOLS')
 --INSERT Appointment_Absence_Entry (ID_3, start_date, start_session, end_date, end_session, Reason, absence_type) values (825, getdate()-1, 'AM', getdate()-1, 'PM', 'just a single day off', 'MAT')
 --INSERT Appointment_Absence_Entry (ID_3, start_date, start_session, end_date, end_session, Reason, absence_type) values (825, getdate(), 'AM', getdate(), 'AM', 'morning', 'HOLS')
 --INSERT Appointment_Absence_Entry (ID_3, start_date, start_session, end_date, end_session, Reason, absence_type) values (825, getdate()+1, 'PM', getdate()+1, 'PM', 'afternoon', 'JURY')
-INSERT Appointment_Absence_Entry (ID_3, start_date, start_session, end_date, end_session, Reason, absence_type) values (876, getdate()-5, 'PM', getdate()-5, 'AM', 'malaria', 'Holiday')
---INSERT Absence_Entry (ID_1, start_date, end_date, Reason, absence_type) values (105, getdate(), getdate(), 'SICK', 'felt bad')
+--INSERT Appointment_Absence_Entry (ID_3, start_date, start_session, end_date, end_session, Reason, absence_type) values (1000, getdate(), 'AM', getdate()+4, 'PM', 'malaria', 'Holiday')
+--INSERT Absence_Entry (ID_1, start_date, end_date, Reason, absence_type) values (116, getdate(), getdate()+2, 'SICK', 'felt bad')
+--INSERT Absence_Entry (ID_1, start_date, end_date, Reason, absence_type) values (116, getdate()-1, getdate()-1, 'SICK', 'felt yucky')
+
+--INSERT Absence_Entry (ID_1, start_date, end_date, Reason, absence_type) values (116, getdate(), getdate(), 'HOLS', 'bit of jolly')
+--INSERT Absence_Entry (ID_1, start_date, end_date, Reason, absence_type) values (117, getdate()-10, getdate()-10, 'HOLS', 'bit of jolly')
+INSERT Absence_Entry (ID_1, start_date, end_date, Reason, absence_type) values (116,'2016-01-01', '2016-01-07', 'HOLS', 'jolly16')
+--INSERT Absence_Entry (ID_1, start_date, end_date, Reason, absence_type) values (116,'2015-01-01', '2015-01-07', 'HOLS', 'bit of jolly')
+
 
 --INSERT Absence_Entry (ID_1, start_date, start_session, end_date, end_session, absence_type, Reason, Absence_In) values (116, getdate()-5, 'PM', getdate()-5, 'AM', 'SICK', 'Jody felt bad', 'Hours')
-INSERT Absence_Entry (ID_1, start_date, start_session, end_date, end_session, absence_type, Reason) values (116, getdate()-10, 'AM', getdate()+10, 'PM', 'sickness', 'bad back')
+--INSERT Absence_Entry (ID_1, start_date, start_session, end_date, end_session, absence_type, Reason) values (116, getdate()-10, 'AM', getdate()+10, 'PM', 'sickness', 'bad back')
 
 PRINT 'UPDATE'
 --UPDATE Absence_Entry SET start_date = start_date - 1
@@ -38,17 +46,33 @@ PRINT 'UPDATE'
 -- DELETE FROM Absence_Entry WHERE ID = 521
 -- DELETE FROM Appointment_Absence_Entry --WHERE ID = 86
 --SELECT * FROM Absence_Entry
---SELECT id,* FROM Appointment_Working_Patterns where id_3 in (835, 876)
---select * from Appointment_Working_Patterns
+
+SELECT id,* FROM Appointments
+SELECT id,* FROM Appointment_Working_Patterns order by Effective_Date --where id_3 in (835, 876)
+--select * from Appointment_Working_Patterns --where id_3 = 876
 --SELECT * FROM Appointment_Absence_Entry
+--SELECT * FROM Absence_Entry
 
-SELECT * FROM Absence_Entry
-SELECT * FROM Absence_Breakdown
-SELECT id, Start_date, Start_Session, End_Date, End_Session, Reason, * FROM Absence
-
+--select * from Working_Patterns
+SELECT * FROM Absence_Breakdown ORDER BY Absence_Date ASC
+--SELECT id, Start_date, Start_Session, End_Date, End_Session, Reason, * FROM Absence
+SELECT ID,Start_Date, Start_Session, End_Date, End_Session, Reason, Absence_Type, Duration_Days, Duration_Hours FROM Absence
 
 --SELECT * FROM ASRSysAccordTransactions
 
 --select * from ASRSysTables order by tablename
 --select * from ASRSysAccordTransferTypes order by ASRBaseTableID
+--SELECT * FROM Appointments where ID =  876
 
+--WHERE wp.Effective_Date <= dr.IndividualDate AND (wp.End_Date >= dr.IndividualDate OR wp.End_Date IS NULL);
+
+
+
+
+--UPDATE @merged
+--		SET End_Date = newData.End_Date
+--		FROM (SELECT TOP 1 m.Effective_Date - 1 AS End_Date FROM @merged m WHERE m.Effective_Date > Effective_Date 
+--		ORDER BY m.Effective_Date) newData
+
+
+		
