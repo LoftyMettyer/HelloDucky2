6e8ae337925f4c00bb7098e7086965176002461c
Mark Edwynn <COA\Mark.Edwynn>
2015-01-19 11:20:15 +0000

    Framework 8.1.11 Build

--- Components/System Framework/Engine/My Project/AssemblyInfo.vb
+++ Components/System Framework/Engine/My Project/AssemblyInfo.vb
@@ -31,7 +31,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("8.1.10.0")> 
+<Assembly: AssemblyVersion("8.1.11.0")> 
 
 
-<Assembly: AssemblyFileVersionAttribute("8.1.10.0")> 
+<Assembly: AssemblyFileVersionAttribute("8.1.11.0")> 
