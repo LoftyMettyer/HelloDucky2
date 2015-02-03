ba58cba9b54bfd8081de6fd2d60e2cef03328507
Lofty Mettyer <COA\lofty.mettyer>
2015-02-03 17:57:32 +0000

    Test harnesses code stubs. No code review required.

--- Database/Custom Post And Establishment/Appointment Insert Test Harness.sql
+++ Database/Custom Post And Establishment/Appointment Insert Test Harness.sql
@@ -1,9 +1,10 @@
 DECLARE @newID integer;
 
-INSERT Appointments (Appointment_Start_Date, ID_219) VALUES (GETDATE(), 164)
-
+INSERT Appointments (Appointment_Start_Date, Appointment_End_Date, ID_219, id_1) VALUES (GETDATE()-365, NULL, 221, 139)
+--update Appointments set actual_hours = actual_hours
 --INSERT Post_Allowances (ID_219, Type, Frequency, Amount, Currency) VALUES (164, 'mytype', 3, 333, 'c')
 --select * from Post_Allowances
+--select * from Post_Records
 
 SELECT TOP 1 @newID = ID FROM Appointments ORDER BY ID DESC
  
@@ -21,3 +22,5 @@ SELECT * FROM Appointment_Working_Patterns WHERE ID_3 = @newID
 -- SELECT * FROM Post_Records where id = 40
 --SELECT * FROM Post_Pension_Schemes
 --select * from Post_Deductions
+select * from Working_Patterns order by id_1
+
