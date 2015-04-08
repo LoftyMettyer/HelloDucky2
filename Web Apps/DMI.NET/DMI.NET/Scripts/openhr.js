7e877293787112fe08fdadad2d5f1b9a1ef55a17
Nick Gibson <COA\nick.gibson>
2015-04-08 16:49:03 +0100

    Bug 16615: Utilities - Workflow defsel now has a Close button

--- Web Apps/DMI.NET/DMI.NET/Scripts/openhr.js
+++ Web Apps/DMI.NET/DMI.NET/Scripts/openhr.js
@@ -80,7 +80,7 @@
 			
 			$('#dialog-confirm').dialog('option', 'buttons', dialogButtons);
 			//$('#dialog-confirm').dialog('option', 'title', title);
-			$('#dialog-confirm p').text(prompt);
+			$('#dialog-confirm p').html($.parseHTML(prompt));
 			$('#dialog-confirm').dialog('open');
 			// If Any ActiveX controls are in the workframeset, move the dailog to the very top of the screen to avoid it being hidden behind the ActiveX
 			if ($('#workframeset object').length > 0) {
