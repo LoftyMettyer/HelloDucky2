f60d12899d75cfda6c295073f560bd58786f688c
Roberto Caballero <COA\roberto.caballero>
2015-04-22 10:04:28 +0100

    Task 17720: Deployment Task for Build 8.1.23

--- Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.1.sql
+++ Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.1.sql
@@ -63228,7 +63228,7 @@ DEALLOCATE curObjects
 GO
 
 		
-DECLARE @sVersion varchar(10) = '8.1.22'
+DECLARE @sVersion varchar(10) = '8.1.23'
 
 EXEC spsys_setsystemsetting 'database', 'version', '8.1';
 EXEC spsys_setsystemsetting 'intranet', 'version', @sVersion;
