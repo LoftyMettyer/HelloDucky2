ec190f5f5b6c6930e5ca05f5bd522b6c87f1d1dd
Nick Gibson <COA\nick.gibson>
2015-06-11 16:00:27 +0100

    Initial Check in

---
+++ Web Apps/DMI.NET/DMI.NET/Controllers/ApiController.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+	Public Class ApiController
+		Inherits Controller
+
+		' GET: Api
+		Function Index() As ActionResult
+
+			If Session("sessionCurrentUser") Is Nothing Then Return New HttpStatusCodeResult(401)
+
+			Return Json("AUTHORIZED", JsonRequestBehavior.AllowGet)
+
+		End Function
+	End Class
+End Namespace
