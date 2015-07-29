4a45fc1947f55f74adc1e17d0a7c1df732f250be
Nick Gibson <COA\nick.gibson>
2015-07-29 12:42:44 +0100

    Welcome message added.

--- Prototypes/OpenHRNexus/OpenHRNexus.WebAPI/Controllers/ResourceController.cs
+++ Prototypes/OpenHRNexus/OpenHRNexus.WebAPI/Controllers/ResourceController.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Http;
 using OpenHRNexus.Service.Interfaces;
 
-namespace OpenHRNexus.WebAPI.Controllers {
-	public class ResourceController : ApiController {
+namespace OpenHRNexus.WebAPI.Controllers
+{
+	public class ResourceController : ApiController
+	{
 		private readonly IWelcomeMessageDataService _welcomeMessageDataService;
 
-		public ResourceController() {
+		public ResourceController()
+		{
 		}
 
-		public ResourceController(IWelcomeMessageDataService welcomeMessageDataService) {
+		public ResourceController(IWelcomeMessageDataService welcomeMessageDataService)
+		{
 			_welcomeMessageDataService = welcomeMessageDataService;
 		}
 
@@ -23,8 +28,17 @@ namespace OpenHRNexus.WebAPI.Controllers {
 		}
 
 		[HttpGet]
-		public IEnumerable<string> GetResourceValue(string guid, string resource)
+		[Authorize]
+		public IEnumerable<string> GetResourceValue(string resource)
 		{
+			//get guid out of jwt claims list
+			var identity = User.Identity as ClaimsIdentity;
+
+			var OpenHRDBGUIDs = (from c in identity.Claims
+													 where c.Type == "OpenHRDBGUID"
+													 select new { c.Value }).FirstOrDefault();
+
+			string OpenHRDBGUID = OpenHRDBGUIDs.Value;
 
 			// TODO - Investigate whether this is the best way to interrogate languages - performance hit?
 			var language = "EN-GB";
@@ -33,7 +47,7 @@ namespace OpenHRNexus.WebAPI.Controllers {
 				language = HttpContext.Current.Request.UserLanguages[0].ToLowerInvariant().Trim();
 			}
 
-			var welcomeMessage = _welcomeMessageDataService.GetWelcomeMessageData(new Guid(guid), language);
+			var welcomeMessage = _welcomeMessageDataService.GetWelcomeMessageData(new Guid(OpenHRDBGUID), language);
 
 			return new string[]
 			{
