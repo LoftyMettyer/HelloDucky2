4ffd50aef051039d1aa75499b03fce44d5be6d32
Lofty Mettyer <COA\lofty.mettyer>
2015-09-03 16:16:48 +0100

    Allday typo

--- Prototypes/OpenHRNexus/Nexus.Common/Models/CalendarEventModel.cs
+++ Prototypes/OpenHRNexus/Nexus.Common/Models/CalendarEventModel.cs
@@ -7,7 +7,7 @@ namespace Nexus.Common.Models
 
         public int id { get; set; }
         public string title { get; set; }
-        public bool llDay { get; set; }
+        public bool allDay { get; set; }
         public DateTime start { get; set; }
         public DateTime end { get; set; }
         public string url { get; set; }
