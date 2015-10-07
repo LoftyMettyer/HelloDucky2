535ab3b904c9eac4ad64a6e607a6f8c40ce88c8f
Lofty Mettyer <COA\lofty.mettyer>
2015-10-07 16:07:06 +0100

    No review required! For some reason this function was not included in the project folder. There will be imminent changes to this file - this is just for code tracability.

---
+++ Database/OpenHR Database/User Defined Functions/udfsys_weekdaysbetweentwodates.sql
@@ -0,0 +1,22 @@
+﻿CREATE FUNCTION [dbo].[udfsys_weekdaysbetweentwodates](
+		@datefrom AS datetime,
+		@dateto AS datetime)
+	RETURNS integer
+	WITH SCHEMABINDING
+	AS
+	BEGIN
+	
+		DECLARE @result integer;
+
+		SELECT @result = CASE 
+			WHEN DATEDIFF (day, @datefrom, @dateto) <= 0 THEN 0
+			ELSE DATEDIFF(day, @datefrom, @dateto + 1) 
+				- (2 * (DATEDIFF(day, @datefrom - (DATEPART(dw, @datefrom) -1),
+					@dateto	- (DATEPART(dw, @dateto) - 1)) / 7))
+				- CASE WHEN DATEPART(dw, @datefrom) = 1 THEN 1 ELSE 0 END
+				- CASE WHEN DATEPART(dw, @dateto) = 7 THEN 1 ELSE 0	END
+				END;
+				
+		RETURN ISNULL(@result,0);
+		
+	END
