7e0f2907d748664b9e716e0f9903dde952dc584d
Lofty Mettyer <COA\lofty.mettyer>
2015-10-07 12:37:27 +0100

    Should have been included in previous changeset after alterations to have the template file embedded in the main asrsysmailmergename table

--- Database/OpenHR Database/Tables/ASRSysMailMergeTemplate.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ASRSysMailMergeTemplate](
-		[Id] [int] IDENTITY(1,1) NOT NULL,
-		[MailMergeID] [int] NOT NULL,
-		[Template] [varbinary](max) NOT NULL,
-		[TemplateName] NVARCHAR(255) NOT NULL, 
-		[UploadDate] [datetime] NOT NULL,
-		[UploadedUser] [nvarchar](255) NOT NULL,
-
-    CONSTRAINT [PK_ASRSysMailMergeTemplate] PRIMARY KEY CLUSTERED ([Id] ASC)) 
