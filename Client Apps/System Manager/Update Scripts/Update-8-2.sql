e28ff4ba80eb58bbffbd819ffbdc950c64f20ed4
Lofty Mettyer <COA\lofty.mettyer>
2015-10-07 16:36:52 +0100

    The weekdays between two dates was not including todays date if the start and end were the same. Requires a system manager update and save. If you have any existing calculations with that function you'll need to run a global update on that field.

--- Client Apps/System Manager/Update Scripts/Update-8-2.sql
+++ Client Apps/System Manager/Update Scripts/Update-8-2.sql
@@ -67,6 +67,35 @@ PRINT 'Step - Audit Changes'
 ALTER TABLE [ASRSysAuditAccess] ALTER COLUMN [ComputerName] varchar(255);
 
 
+/* ------------------------------------------------------- */
+PRINT 'Step - Calculation Changes'
+/* ------------------------------------------------------- */
+
+	IF EXISTS (SELECT * FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[udfsys_weekdaysbetweentwodates]') AND xtype in (N'FN', N'IF', N'TF'))
+		DROP FUNCTION [dbo].[udfsys_weekdaysbetweentwodates];
+
+	EXEC sp_executesql N'CREATE FUNCTION [dbo].[udfsys_weekdaysbetweentwodates](
+			@datefrom AS datetime,
+			@dateto AS datetime)
+		RETURNS integer
+		WITH SCHEMABINDING
+		AS
+		BEGIN
+	
+			DECLARE @result integer;
+
+			SELECT @result = CASE 
+				WHEN DATEDIFF (day, @datefrom, @dateto) <= 0 THEN 0
+				ELSE (DATEDIFF(dd, @datefrom, @dateto) + 1)
+					- (DATEDIFF(wk, @datefrom, @dateto) * 2)
+					- (CASE WHEN DATEPART(dw, @datefrom) = 1 THEN 1 ELSE 0 END)
+					- (CASE WHEN DATEPART(dw, @dateto) = 7 THEN 1 ELSE 0 END)
+					END;
+				
+			RETURN ISNULL(@result,0);
+		
+		END'
+
 
 /* ------------------------------------------------------- */
 PRINT 'Step - Export additions'
--- Database/OpenHR Database/User Defined Functions/udfsys_weekdaysbetweentwodates.sql
+++ Database/OpenHR Database/User Defined Functions/udfsys_weekdaysbetweentwodates.sql
@@ -10,11 +10,10 @@
 
 		SELECT @result = CASE 
 			WHEN DATEDIFF (day, @datefrom, @dateto) <= 0 THEN 0
-			ELSE DATEDIFF(day, @datefrom, @dateto + 1) 
-				- (2 * (DATEDIFF(day, @datefrom - (DATEPART(dw, @datefrom) -1),
-					@dateto	- (DATEPART(dw, @dateto) - 1)) / 7))
-				- CASE WHEN DATEPART(dw, @datefrom) = 1 THEN 1 ELSE 0 END
-				- CASE WHEN DATEPART(dw, @dateto) = 7 THEN 1 ELSE 0	END
+			ELSE (DATEDIFF(dd, @datefrom, @dateto) + 1)
+				- (DATEDIFF(wk, @datefrom, @dateto) * 2)
+				- (CASE WHEN DATEPART(dw, @datefrom) = 1 THEN 1 ELSE 0 END)
+				- (CASE WHEN DATEPART(dw, @dateto) = 7 THEN 1 ELSE 0 END)
 				END;
 				
 		RETURN ISNULL(@result,0);
