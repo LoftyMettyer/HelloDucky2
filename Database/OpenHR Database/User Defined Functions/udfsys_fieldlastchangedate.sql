25adf969a2b9d69c944c39ffdbac7acd137d3fd3
Lofty Mettyer <COA\lofty.mettyer>
2015-10-08 13:52:22 +0100

    No review required. This is adding the existing udfsys_fieldlastchangedate function which for some reason was omitted from the project.

---
+++ Database/OpenHR Database/User Defined Functions/udfsys_fieldlastchangedate.sql
@@ -0,0 +1,24 @@
+﻿CREATE FUNCTION [dbo].[udfsys_fieldlastchangedate](
+		@colrefID	varchar(32),
+		@recordID	integer
+	)
+	RETURNS datetime
+	WITH SCHEMABINDING
+	AS
+	BEGIN
+
+		DECLARE @result		datetime,
+				@tableid	integer,
+				@columnid	integer;
+		
+		SET @tableid = SUBSTRING(@colrefID, 1, 8);
+		SET @columnid = SUBSTRING(@colrefID, 10, 8);
+
+		SELECT TOP 1 @result = [DateTimeStamp] FROM dbo.[ASRSysAuditTrail]
+			WHERE [ColumnID] = @columnid AND [TableID] = @tableID
+				AND @recordID = [RecordID]
+			ORDER BY [DateTimeStamp] DESC ;
+
+		RETURN @result;
+
+	END
