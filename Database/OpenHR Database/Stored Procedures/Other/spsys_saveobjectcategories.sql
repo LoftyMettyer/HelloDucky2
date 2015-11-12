de4f98c18d88d55fb50602f33836b08997243606
Lofty Mettyer <COA\lofty.mettyer>
2015-11-12 12:48:58 +0000

    Housekeeping - spsys_saveobjectcategories was not included in the database project. This was included in the 5.1 fat client update script and so has already been rolled out to a number of customers.

---
+++ Database/OpenHR Database/Stored Procedures/Other/spsys_saveobjectcategories.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spsys_saveobjectcategories](@utilityType as integer, @UtilityID as integer, @CategoryID integer)
+		AS
+		BEGIN
+
+			SET NOCOUNT ON;
+
+			DELETE FROM dbo.tbsys_objectcategories
+				WHERE [objecttype] = @utilityType AND [objectid] = @UtilityID;
+			
+			IF @CategoryID > 0
+			BEGIN
+				INSERT dbo.tbsys_objectcategories([objecttype], [objectid], [categoryid])
+					VALUES (@utilityType, @UtilityID, @CategoryID);
+			END
+
+		END
