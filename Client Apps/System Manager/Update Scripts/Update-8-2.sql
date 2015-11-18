0306d3384a6ace95774eb5cac069297130fa9fc4
Lofty Mettyer <COA\lofty.mettyer>
2015-11-18 14:13:50 +0000

    The spstat_getaudittrail prcedure is source control was out of date and hence the changes to tbsys_columns -> ASRSysColumn was not reflect in this proc. Code taken from the 5.1 update script, modified and placed in the current update script. Function is called from teh security manager audit trail functionality.

--- Client Apps/System Manager/Update Scripts/Update-8-2.sql
+++ Client Apps/System Manager/Update Scripts/Update-8-2.sql
@@ -2389,6 +2389,86 @@ PRINT 'Step - Misc Updates'
 	END';
 
 
+
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spstat_getaudittrail]') AND xtype = 'P')
+		DROP PROCEDURE [dbo].[spstat_getaudittrail];
+		
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spstat_getaudittrail] (
+		@piAuditType	int,
+		@psOrder 		varchar(MAX),
+		@psFilter		varchar(MAX),
+		@piTop			int)
+	AS
+	BEGIN
+
+		SET NOCOUNT ON;
+
+		DECLARE @sSQL nvarchar(MAX);
+
+		IF @piAuditType = 1
+		BEGIN
+			SET @sSQL = ''SELECT {TOP} 
+				a.userName AS [User], 
+				a.dateTimeStamp AS [Date / Time], 
+				a.tableName AS [Table], 
+				a.columnName AS [Column], 
+				a.oldValue AS [Old Value], 
+				a.newValue AS [New Value], 
+				a.recordDesc AS [Record Description],
+				a.id,
+				CASE WHEN c.DataType = 2 OR c.DataType = 4 THEN 1 ELSE 0 END AS IsNumeric
+				FROM dbo.ASRSysAuditTrail a
+				LEFT JOIN ASRSysColumns c ON c.ColumnID = a.ColumnID'';
+		END
+		ELSE IF @piAuditType = 2
+			SET @sSQL =  ''SELECT {TOP} 
+				a.userName AS [User], 
+				a.dateTimeStamp AS [Date / Time],
+				a.groupName AS [User Group],
+				a.viewTableName AS [View / Table],
+				a.columnName AS [Column], 
+				a.action AS [Action],
+				a.permission AS [Permission], 
+				a.id
+				FROM dbo.ASRSysAuditPermissions a'';
+		ELSE IF @piAuditType = 3
+			SET @sSQL = ''SELECT {TOP} 
+				a.userName AS [User],
+    			a.dateTimeStamp AS [Date / Time],
+				a.groupName AS [User Group], 
+				a.userLogin AS [User Login],
+				a.[Action], 
+				a.id
+				FROM dbo.ASRSysAuditGroup a'';
+		ELSE IF @piAuditType = 4
+			SET @sSQL = ''SELECT {TOP} 
+				a.DateTimeStamp AS [Date / Time],
+				a.UserGroup AS [User Group],
+				a.UserName AS [User], 
+				a.ComputerName AS [Computer Name],
+				a.HRProModule AS [Module],
+				a.Action AS [Action], 
+				a.id
+				FROM dbo.ASRSysAuditAccess a'';
+				
+		IF LEN(@psFilter) > 0
+			SET @sSQL = @sSQL + CHAR(10) + ''WHERE '' + @psFilter;
+
+		IF LEN(@psOrder) > 0
+			SET @sSQL = @sSQL + CHAR(10) + @psOrder;
+				
+		-- Retreive selected data
+		IF LEN(@sSQL) > 0 
+		BEGIN
+			IF ISNULL(@piTop, 0) > 0
+				SET @sSQL = REPLACE(@sSQL, ''{TOP}'', ''TOP '' + convert(varchar, @piTop));
+				
+			EXECUTE sp_executeSQL @sSQL;
+		END
+
+	END';
+
+
 PRINT 'Final Step - Updating Versions'
 
 	EXEC spsys_setsystemsetting 'database', 'version', '8.2';
--- Database/OpenHR Database/Stored Procedures/Other/spstat_getaudittrail.sql
+++ Database/OpenHR Database/Stored Procedures/Other/spstat_getaudittrail.sql
@@ -1,87 +1,74 @@
 CREATE PROCEDURE [dbo].[spstat_getaudittrail] (
-	@piAuditType	int,
-	@psOrder 		varchar(MAX))
-AS
-BEGIN
-
-	SET NOCOUNT ON;
-
-	DECLARE @sSQL			varchar(MAX),
-			@sExecString	nvarchar(MAX);
-
-	IF @piAuditType = 1
+		@piAuditType	int,
+		@psOrder 		varchar(MAX),
+		@psFilter		varchar(MAX),
+		@piTop			int)
+	AS
 	BEGIN
 
-		SET @sSQL = 'SELECT userName AS [User], 
-			dateTimeStamp AS [Date / Time], 
-			tableName AS [Table], 
-			columnName AS [Column], 
-			oldValue AS [Old Value], 
-			newValue AS [New Value], 
-			recordDesc AS [Record Description],
-			id
-			FROM dbo.ASRSysAuditTrail ';
+		SET NOCOUNT ON;
+
+		DECLARE @sSQL nvarchar(MAX);
+
+		IF @piAuditType = 1
+		BEGIN
+			SET @sSQL = 'SELECT {TOP} 
+				a.userName AS [User], 
+				a.dateTimeStamp AS [Date / Time], 
+				a.tableName AS [Table], 
+				a.columnName AS [Column], 
+				a.oldValue AS [Old Value], 
+				a.newValue AS [New Value], 
+				a.recordDesc AS [Record Description],
+				a.id,
+				CASE WHEN c.DataType = 2 OR c.DataType = 4 THEN 1 ELSE 0 END AS IsNumeric
+				FROM dbo.ASRSysAuditTrail a
+				LEFT JOIN ASRSysColumns c ON c.ColumnID = a.ColumnID';
+		END
+		ELSE IF @piAuditType = 2
+			SET @sSQL =  'SELECT {TOP} 
+				a.userName AS [User], 
+				a.dateTimeStamp AS [Date / Time],
+				a.groupName AS [User Group],
+				a.viewTableName AS [View / Table],
+				a.columnName AS [Column], 
+				a.action AS [Action],
+				a.permission AS [Permission], 
+				a.id
+				FROM dbo.ASRSysAuditPermissions a';
+		ELSE IF @piAuditType = 3
+			SET @sSQL = 'SELECT {TOP} 
+				a.userName AS [User],
+    			a.dateTimeStamp AS [Date / Time],
+				a.groupName AS [User Group], 
+				a.userLogin AS [User Login],
+				a.[Action], 
+				a.id
+				FROM dbo.ASRSysAuditGroup a';
+		ELSE IF @piAuditType = 4
+			SET @sSQL = 'SELECT {TOP} 
+				a.DateTimeStamp AS [Date / Time],
+				a.UserGroup AS [User Group],
+				a.UserName AS [User], 
+				a.ComputerName AS [Computer Name],
+				a.HRProModule AS [Module],
+				a.Action AS [Action], 
+				a.id
+				FROM dbo.ASRSysAuditAccess a';
+				
+		IF LEN(@psFilter) > 0
+			SET @sSQL = @sSQL + CHAR(10) + 'WHERE ' + @psFilter;
 
 		IF LEN(@psOrder) > 0
-			SET @sExecString = @sSQL + @psOrder;
-		ELSE
-			SET @sExecString = @sSQL;
-		
-	END
-	ELSE IF @piAuditType = 2
-	BEGIN
-
-		SET @sSQL =  'SELECT userName AS [User], 
-			dateTimeStamp AS [Date / Time],
-			groupName AS [User Group],
-			viewTableName AS [View / Table],
-			columnName AS [Column], 
-			action AS [Action],
-			permission AS [Permission], 
-			id
-			FROM dbo.ASRSysAuditPermissions ';
-
-		IF LEN(@psOrder) > 0
-			SET @sExecString = @sSQL + @psOrder;
-		ELSE
-			SET @sExecString = @sSQL;
+			SET @sSQL = @sSQL + CHAR(10) + @psOrder;
+				
+		-- Retreive selected data
+		IF LEN(@sSQL) > 0 
+		BEGIN
+			IF ISNULL(@piTop, 0) > 0
+				SET @sSQL = REPLACE(@sSQL, '{TOP}', 'TOP ' + convert(varchar, @piTop));
+				
+			EXECUTE sp_executeSQL @sSQL;
+		END
 
 	END
-	ELSE IF @piAuditType = 3
-	BEGIN
-		SET @sSQL = 'SELECT userName AS [User],
-    			dateTimeStamp AS [Date / Time],
-			groupName AS [User Group], 
-			userLogin AS [User Login],
-			[Action], 
-			id
-			FROM dbo.ASRSysAuditGroup ';
-
-		IF LEN(@psOrder) > 0
-			SET @sExecString = @sSQL + @psOrder;
-		ELSE
-			SET @sExecString = @sSQL;
-
-	END
-	ELSE IF @piAuditType = 4
-	BEGIN
-		SET @sSQL = 'SELECT DateTimeStamp AS [Date / Time],
-    			UserGroup AS [User Group],
-			UserName AS [User], 
-			ComputerName AS [Computer Name],
-			HRProModule AS [Module],
-			Action AS [Action], 
-			id
-			FROM dbo.ASRSysAuditAccess ';
-
-		IF LEN(@psOrder) > 0
-			SET @sExecString = @sSQL + @psOrder;
-		ELSE
-			SET @sExecString = @sSQL;
-
-	END
-
-	-- Retreive selected data
-	IF LEN(@sExecString) > 0 EXECUTE sp_executeSQL @sExecString;
-
-END
