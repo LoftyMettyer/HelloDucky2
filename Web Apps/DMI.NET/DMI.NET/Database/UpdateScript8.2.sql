ab417b926e866770e59142f892b620aff3fdf9fc
Roberto Caballero <COA\roberto.caballero>
2015-12-09 15:47:49 +0000

    Task 21291: Deployment Task for OpenHR Build 8.2.7

--- Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.2.sql
+++ Web Apps/DMI.NET/DMI.NET/Database/UpdateScript8.2.sql
@@ -62805,7 +62805,7 @@ DEALLOCATE curObjects
 GO
 
 		
-DECLARE @sVersion varchar(10) = '8.2.6'
+DECLARE @sVersion varchar(10) = '8.2.7'
 
 EXEC spsys_setsystemsetting 'intranet', 'version', @sVersion;
 EXEC spsys_setsystemsetting 'ssintranet', 'version', @sVersion;
--- Web Apps/DMI.NET/DMI.NET/My Project/AssemblyInfo.vb
+++ Web Apps/DMI.NET/DMI.NET/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("8.2.6")> 
-<Assembly: AssemblyFileVersion("8.2.6")> 
+<Assembly: AssemblyVersion("8.2.7")> 
+<Assembly: AssemblyFileVersion("8.2.7")> 
