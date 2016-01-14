87ff69ca9a03f927e134e3574d6ac41a9a9bcf69
Roberto Caballero <COA\roberto.caballero>
2016-01-14 15:19:45 +0000

    Task 19904: Elevated command prompt privileges required to install Workflow Website.msi

---
+++ Workflow/OpenHR Workflow Web App/OpenHRWorkflow Wix Extensions/WixFileVersionExtension.vb
@@ -0,0 +1,16 @@
+﻿Imports Microsoft.Tools.WindowsInstallerXml
+Public Class WixFileVersionExtension
+	Inherits WixExtension
+
+	Private _versionPreprocessorExtension As WixFileVersionPreprocessorExtension
+
+	Public Overrides ReadOnly Property PreprocessorExtension As PreprocessorExtension
+		Get
+			'If we haven't create the preprocessor then do it now
+			If _versionPreprocessorExtension Is Nothing Then
+				_versionPreprocessorExtension = New WixFileVersionPreprocessorExtension
+			End If
+			Return _versionPreprocessorExtension
+		End Get
+	End Property
+End Class
