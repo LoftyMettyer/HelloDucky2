1732fbbb138d3be58c327b13d0dce977031fe99c
Lofty Mettyer <COA\lofty.mettyer>
2016-02-05 11:33:20 +0000

    Added spadmin_optimiserecordsave which was added in 4.3 but was never included in the database project (or the old style SourceSafe for that matter). It then needed amending from tbsys_tables to ASRSysTables following some db hardening work.

--- Client Apps/System Manager/Update Scripts/Update-8-2.sql
+++ Client Apps/System Manager/Update Scripts/Update-8-2.sql
@@ -2245,6 +2245,26 @@ END
 		END
 	END'
 
+	IF EXISTS (SELECT *	FROM dbo.sysobjects	WHERE id = object_id(N'[dbo].[spadmin_optimiserecordsave]') AND xtype = 'P')
+		DROP PROCEDURE [dbo].[spadmin_optimiserecordsave];
+
+	EXECUTE sp_executeSQL N'CREATE PROCEDURE [dbo].[spadmin_optimiserecordsave]
+		AS
+		BEGIN
+
+			SET NOCOUNT ON;
+
+			DECLARE @sCode nvarchar(MAX);
+
+			SET @sCode = '''';
+			SELECT @sCode = @sCode + ''UPDATE dbo.[tbuser_'' + [tablename] + ''] SET [updflag] = 0 WHERE [id] = 0;'' + CHAR(13)
+				FROM ASRSysTables
+				WHERE [TableType] IN (1,2)
+				ORDER BY [tabletype];
+
+			EXECUTE sp_executesql @sCode;
+	
+		END';
 
 /* ------------------------------------------------------- */
 PRINT 'Step - Database Hardening'
---
+++ Database/OpenHR Database/Stored Procedures/System Manager/spadmin_optimiserecordsave.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[spadmin_optimiserecordsave]
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	DECLARE @sCode nvarchar(MAX);
+
+	SET @sCode = '';
+	SELECT @sCode = @sCode + 'UPDATE dbo.[tbuser_' + [tablename] + '] SET [updflag] = 0 WHERE [id] = 0;' + CHAR(13)
+		FROM ASRSysTables
+		WHERE [TableType] IN (1,2)
+		ORDER BY [tabletype];
+
+	EXECUTE sp_executesql @sCode;
+	
+END
