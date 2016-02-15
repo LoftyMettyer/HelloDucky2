f2624d35f4df7396127c53cf8a2e849ba171db79
Lofty Mettyer <COA\lofty.mettyer>
2016-02-15 17:13:25 +0000

    Items following code review. @sJobTypeName isn't used in that section of code so worked just fine before, but its a good idea to sort this code out as it could be a ticking timebomb if we beef up batchjobs.

--- Database/OpenHR Database/Stored Procedures/Web/sp_ASRIntDefUsage.sql
+++ Database/OpenHR Database/Stored Procedures/Web/sp_ASRIntDefUsage.sql
@@ -70,7 +70,7 @@ BEGIN
 		IF @intType = 9 SET @sJobTypeName = 'MAIL MERGE' 
 		IF @intType = 17 SET @sJobTypeName = 'CALENDAR REPORT'
 		IF @intType = 35 SET @sJobTypeName = '9-BOX GRID REPORT'
-		IF @intType = 35 SET @sJobTypeName = 'TALENT REPORT'
+		IF @intType = 38 SET @sJobTypeName = 'TALENT REPORT'
 		
 		DECLARE usage_cursor CURSOR LOCAL FAST_FORWARD FOR 
 			SELECT DISTINCT ASRSysBatchJobName.Name, 
