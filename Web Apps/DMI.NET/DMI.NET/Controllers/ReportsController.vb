11225297ffc9b1d025c4b4fe7c17bdba07e60373
Prashant Shah <COA\prashant.shah>
2016-02-18 10:59:32 +0000

    Decode the warning message.

--- Web Apps/DMI.NET/DMI.NET/Controllers/ReportsController.vb
+++ Web Apps/DMI.NET/DMI.NET/Controllers/ReportsController.vb
@@ -132,21 +132,21 @@ Namespace Controllers
                     End If
                 Next
 
-                ' Check the column headings are unique.
-                Dim breakNestedLoop As Boolean
-                For Each columnItem As ReportColumnItem In objModel.Columns
-                    For Each columnItemHeaderToCheck As ReportColumnItem In objModel.Columns
+				' Check the column headings are unique.
+				Dim breakNestedLoop As Boolean
+				For Each columnItem As ReportColumnItem In objModel.Columns
+					For Each columnItemHeaderToCheck As ReportColumnItem In objModel.Columns
 
-                        If columnItem.ID <> columnItemHeaderToCheck.ID AndAlso UCase(columnItem.Heading.Trim()) = UCase(columnItemHeaderToCheck.Heading.Trim()) AndAlso columnItemHeaderToCheck.IsHidden = False Then
-                            ModelState.AddModelError("IsColumnHeaderUnique", "One or more columns / calculations in your report have a heading of '" & columnItemHeaderToCheck.Heading & "'. " & "Column headings must be unique.")
-                            breakNestedLoop = True
-                            Exit For
-                        End If
-                    Next
-                    If breakNestedLoop Then
-                        Exit For
-                    End If
-                Next
+						If columnItem.ID <> columnItemHeaderToCheck.ID AndAlso UCase(columnItem.Heading.Trim()) = UCase(columnItemHeaderToCheck.Heading.Trim()) AndAlso columnItemHeaderToCheck.IsHidden = False Then
+                            ModelState.AddModelError("IsColumnHeaderUnique", "One or more columns / calculations in your report have a heading of '" & HttpUtility.UrlDecode(columnItemHeaderToCheck.Heading) & "'. " & "Column headings must be unique.")
+							breakNestedLoop = True
+							Exit For
+						End If
+					Next
+					If breakNestedLoop Then
+						Exit For
+					End If
+				Next
 
                 If objModel.IsSummary AndAlso objModel.Columns.Where(Function(m) m.IsAverage OrElse m.IsCount OrElse m.IsTotal).LongCount() = 0 Then
                     ModelState.AddModelError("IsSummaryOK", "There are no columns defined as aggregates for this summary report.")
@@ -314,7 +314,7 @@ Namespace Controllers
 					For Each columnItemHeaderToCheck As ReportColumnItem In objModel.Columns
 
 						If columnItem.ID <> columnItemHeaderToCheck.ID AndAlso UCase(columnItem.Heading.Trim()) = UCase(columnItemHeaderToCheck.Heading.Trim()) AndAlso columnItemHeaderToCheck.IsHidden = False Then
-							ModelState.AddModelError("IsColumnHeaderUnique", "One or more columns in your report have a heading of '" & columnItemHeaderToCheck.Heading & "'. " & "Column headings must be unique.")
+							ModelState.AddModelError("IsColumnHeaderUnique", "One or more columns in your report have a heading of '" & HttpUtility.UrlDecode(columnItemHeaderToCheck.Heading) & "'. " & "Column headings must be unique.")
 							breakNestedLoop = True
 							Exit For
 						End If
