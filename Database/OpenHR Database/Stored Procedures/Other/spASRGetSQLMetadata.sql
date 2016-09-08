3009487985d3e632ccedfe89b87a91e9f1c2f71c
Nick Gibson <COA\nick.gibson>
2016-09-08 10:01:12 +0100

    Bug 23821: Workflow Website and Service check for a different resolution of server name

---
+++ Database/OpenHR Database/Stored Procedures/Other/spASRGetSQLMetadata.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spASRGetSQLMetadata](
+	@sServerName nvarchar(128) OUTPUT,
+	@sDBName nvarchar(128) OUTPUT)
+	AS
+	BEGIN
+			SET @sServerName = CONVERT(nvarchar(128), SERVERPROPERTY('ServerName'));
+			SET @sDBName = db_name();
+	END
