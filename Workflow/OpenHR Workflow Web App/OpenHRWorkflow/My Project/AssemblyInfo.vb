ff1af1543ea3652a0195564fdaf7092dafcf4306
Roberto Caballero <COA\roberto.caballero>
2016-10-27 11:27:13 +0100

    Task 24897: Build Task for OpenHR Workflow 8.3.5

--- Workflow/OpenHR Workflow Web App/OpenHRWorkflow/My Project/AssemblyInfo.vb
+++ Workflow/OpenHR Workflow Web App/OpenHRWorkflow/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("8.3.3")> 
-<Assembly: AssemblyFileVersion("8.3.3")> 
+<Assembly: AssemblyVersion("8.3.5")> 
+<Assembly: AssemblyFileVersion("8.3.5")> 
