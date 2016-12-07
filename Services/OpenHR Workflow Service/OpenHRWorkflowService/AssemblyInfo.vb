b3c278d14d88352cbaf12625bf6ec9ba08c22ec2
Roberto Caballero <COA\roberto.caballero>
2016-12-07 14:26:18 +0000

    Task 25305 - Build Task for OpenHR Workflow Service 8.3.7

--- Services/OpenHR Workflow Service/OpenHRWorkflowService/AssemblyInfo.vb
+++ Services/OpenHR Workflow Service/OpenHRWorkflowService/AssemblyInfo.vb
@@ -29,6 +29,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("8.3.6.00")> 
-
-<Assembly: AssemblyFileVersionAttribute("8.3.6.00")> 
+<Assembly: AssemblyVersion("8.3.7")>
+<Assembly: AssemblyFileVersionAttribute("8.3.7")>
